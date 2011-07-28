57298ca077131bd4ae35eeaa7aae9c4e338bcf75
Ed Cheng <ed@boylesoftware.com>
2011-07-28 14:40:12 +0000

    removed

--- db/v1/recreate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh 
-
-
-echo drop database wasp | /vol0/mysql/bin/mysql -u root --password=5n1ck3l
-echo create database wasp | /vol0/mysql/bin/mysql -u root --password=5n1ck3l
-
-
-/vol0/mysql/bin/mysql -u wasp --password=waspV2 wasp < ~/wasp/db/create.sql
