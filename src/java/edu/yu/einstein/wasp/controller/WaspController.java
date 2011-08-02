517b0742835b7d1203817d35913c25610a838081
Ed Cheng <ed@boylesoftware.com>
2011-08-02 21:49:24 +0000

    parent class

--- src/java/edu/yu/einstein/wasp/controller/WaspController.java
+++ src/java/edu/yu/einstein/wasp/controller/WaspController.java
@@ -1,8 +1,5 @@
 package edu.yu.einstein.wasp.controller;
 
-import org.springframework.web.servlet.mvc.Controller;
-import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,56 +7,29 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import java.io.IOException;
-import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 
-import edu.yu.einstein.wasp.dao.UserDao;
-import edu.yu.einstein.wasp.dao.impl.UserDaoImpl;
 import edu.yu.einstein.wasp.model.User;
+import edu.yu.einstein.wasp.service.UserService;
 
-import javax.persistence.*;
+@Controller
+public class WaspController {
 
-public class WaspController implements Controller {
+  protected final Log logger = LogFactory.getLog(getClass());
 
-    protected final Log logger = LogFactory.getLog(getClass());
+  @Autowired
+  protected UserService userService;
 
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+  public User getAuthenticatedUser() { 
+    Authentication authentication = SecurityContextHolder.getContext()
+                                .getAuthentication();
 
-        logger.info("Returning wasp view");
+    User user = this.userService.getUserByLogin(authentication.getName());
 
-        String now = (new Date()).toString();
+    return user;
+  }
 
-        EntityManager em = null;
-        User user = null;
-
-        try{
-          EntityManagerFactory emf = Persistence.createEntityManagerFactory("wasp1");
-          em = emf.createEntityManager();
-
-          UserDao userDao  = new UserDaoImpl();
-          userDao.setEntityManager(em);
-    
-          EntityTransaction t = em.getTransaction();
-          t.begin();
-    
-          user = userDao.getUserByUserId(28);
-        } catch (Exception e) {
-          System.out.println("YYYYYYYY");
-          System.out.println(e.getMessage());
-          System.out.println("ZZZZZZZZ");
-          e.printStackTrace();
-        } finally {
-          if (em != null) {
-          em.flush();
-          em.close();
-          }
-        }
-
-        Map<String, Object> viewModel = new HashMap<String, Object>();
-        viewModel.put("now", now);
-        viewModel.put("user", user);
-
-        return new ModelAndView("wasp", "model", viewModel);
-    }
 }
