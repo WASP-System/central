b982074371c017babca5e6686d3aa78a64ed4317
Ed Cheng <ed@boylesoftware.com>
2011-08-09 21:20:04 +0000

    added jobuser

--- src/java/edu/yu/einstein/wasp/controller/JobController.java
+++ src/java/edu/yu/einstein/wasp/controller/JobController.java
@@ -9,8 +9,11 @@ import javax.persistence.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,12 +24,14 @@ import java.util.Date;
 import java.util.List; 
 
 import edu.yu.einstein.wasp.service.JobService;
+import edu.yu.einstein.wasp.service.JobUserService;
+import edu.yu.einstein.wasp.service.RoleService;
 import edu.yu.einstein.wasp.model.*;
 
 @Controller
 @Transactional
 @RequestMapping("/job")
-public class JobController {
+public class JobController extends WaspController {
 
   private JobService jobService;
   @Autowired
@@ -35,6 +40,24 @@ public class JobController {
   }
   public JobService getJobService() {
     return this.jobService;
+  }
+
+  private JobUserService jobUserService;
+  @Autowired
+  public void setJobUserService(JobUserService jobUserService) {
+    this.jobUserService = jobUserService;
+  }
+  public JobUserService getJobUserService() {
+    return this.jobUserService;
+  }
+
+  private RoleService roleService;
+  @Autowired
+  public void setJobUserService(RoleService roleService) {
+    this.roleService = roleService;
+  }
+  public RoleService getRoleUserService() {
+    return this.roleService;
   }
 
   @RequestMapping("/list")
@@ -46,18 +69,12 @@ public class JobController {
     return "job/list";
   }
 
-  @RequestMapping(value="/detail/{strId}", method=RequestMethod.GET)
-  public String detail(@PathVariable("strId") String strId, ModelMap m) {
+  @RequestMapping(value="/detail/{jobId}", method=RequestMethod.GET)
+  public String detail(@PathVariable("jobId") Integer jobId, ModelMap m) {
     String now = (new Date()).toString();
 
-    Integer i;
-    try {
-      i = new Integer(strId);
-    } catch (Exception e) {
-      return "default";
-    }
 
-    Job job = this.getJobService().getById(i.intValue());
+    Job job = this.getJobService().getById(jobId);
 
     List<JobMeta> jobMetaList = job.getJobMeta();
     jobMetaList.size();
@@ -68,6 +85,9 @@ public class JobController {
     List<JobFile> jobFileList = job.getJobFile();
     jobFileList.size();
 
+    List<JobUser> jobUserList = job.getJobUser();
+    jobUserList.size();
+
     List<Statejob> stateJobList = job.getStatejob();
     stateJobList.size();
 
@@ -76,10 +96,57 @@ public class JobController {
     m.addAttribute("jobmeta", jobMetaList);
     m.addAttribute("jobsample", jobSampleList);
     m.addAttribute("jobfile", jobFileList);
+    m.addAttribute("jobuser", jobUserList);
     m.addAttribute("statejob", stateJobList);
 
     return "job/detail";
   }
+
+
+  @RequestMapping(value="/user/roleAdd", method=RequestMethod.POST)
+  @PreAuthorize("hasRole('god') or hasRole('lm-' + #labId)")
+  public String jobViewerUserRoleAdd (
+      @RequestParam("labId") Integer labId,
+      @RequestParam("jobId") Integer jobId,
+      @RequestParam("useremail") String useremail,
+      ModelMap m) {
+ 
+    // todo ensure usermail exists
+    // todo jobid within labid
+    // todo not already job submitter
+
+    Role role = roleService.getRoleByRoleName("jv");
+
+    User user = userService.getUserByEmail(useremail);
+    JobUser jobUser = new JobUser();
+    jobUser.setJobId(jobId);
+    jobUser.setUserId(user.getUserId());
+    jobUser.setRoleId(role.getRoleId());
+    jobUserService.save(jobUser);
+
+    return "redirect:/job/detail/" + jobId + ".do";
+  }
+
+
+  @RequestMapping(value="/user/roleRemove/{labId}/{jobId}/{userId}", method=RequestMethod.GET)
+  @PreAuthorize("hasRole('god') or hasRole('lm-' + #labId)")
+  public String departmentUserRoleRemove (
+      @PathVariable("labId") Integer labId,
+      @PathVariable("jobId") Integer jobId,
+      @PathVariable("userId") Integer userId,
+      ModelMap m) {
+  
+    JobUser jobUser = jobUserService.getJobUserByJobIdUserId(jobId, userId);
+
+    // todo check job within lab
+    // check user is just a job viewer
+
+    jobUserService.remove(jobUser);
+
+    return "redirect:/job/detail/" + jobId + ".do";
+  }
+
+
 
 
 }
