4323ee9db2681a0ce43dcfc31389f5aed882f3b3
Ed Cheng <ed@boylesoftware.com>
2011-08-20 03:58:11 +0000

    meta -> Meta

--- src/java/edu/yu/einstein/wasp/controller/MetaValidator.java
+++ src/java/edu/yu/einstein/wasp/controller/MetaValidator.java
@@ -19,62 +19,53 @@ import edu.yu.einstein.wasp.model.MetaAttribute;
  */
 
 public class MetaValidator {
-	  	
- 		public enum Constraint {
- 			NotEmpty,
- 			Regexp
- 		}
-	
-		 private Map<String, String> map=new HashMap<String, String>(); 
-		
-	     public MetaValidator(String... pairs)  {
-	    	 
-	    	 if (pairs.length % 2!=0) throw new IllegalStateException("Number of params must be even");
-	    	 
-	    	 for(int i=0;i<pairs.length;i+=2) {
-	    		 map.put(pairs[i],pairs[i+1]);
-	    	 }
-	    	 
-	     }
-	    	
-	    public void validate(List<? extends MetaBase> list, BindingResult result, MetaAttribute.Area area) {
-	    	
-	    	Errors errors=new BindException(result.getTarget(), area.name()); 
-	    		    	
-	    	for(int i=0;i<list.size();i++) {
-	    		MetaBase meta=list.get(i);
-	    		String constraint=map.get(meta.getK());
-	    		if (constraint==null) continue;
-	    		
-	    		if (constraint.startsWith(Constraint.Regexp.name()+":")) {
-	    			
-	    			 //TODO: optimize for speed (pre-compile) if used outside of admin pages
-	    			 String regexp=constraint.substring(Constraint.Regexp.name().length()+1); 
-	    			 Pattern p = Pattern.compile(regexp);
-	    			 Matcher m = p.matcher(meta.getV());
-	    			 boolean b = m.matches();
-	    			 
-	    				    			 
-	    			 if (!b) {
-	    				 errors.rejectValue(area+"meta["+i+"].k", area+"."+meta.getK().replace(area+".", "")+".error");  
-	    			 }
-	    				    			 
-	    		} else if (constraint.equals(Constraint.NotEmpty.name())){
-	    			if (meta.getV()==null || meta.getV().isEmpty()) {
-	    				
-	    				
-	    				String fieldName=area+"meta["+i+"].k";
-	    				String errorMessageKey=area+"."+meta.getK().replace(area+".", "")+".error";
-	    				errors.rejectValue(fieldName,errorMessageKey );   				 
-	    				
-	    			}	    			 
-	    		} else {
-	    			throw new IllegalStateException("Unknown constraint "+constraint+"|"+meta);
-	    		}
-	    		
-	    		
-	    		
-	    	}
-	    	result.addAllErrors(errors);
-	    }
+	 
+	public enum Constraint {
+		NotEmpty,
+		Regexp
 	}
+	
+	private Map<String, String> map=new HashMap<String, String>(); 
+		
+	public MetaValidator(String... pairs)  {
+		if (pairs.length % 2!=0) throw new IllegalStateException("Number of params must be even");
+			
+		for(int i=0;i<pairs.length;i+=2) {
+			map.put(pairs[i],pairs[i+1]);
+		}
+	
+	}
+	
+	public void validate(List<? extends MetaBase> list, BindingResult result, MetaAttribute.Area area) {
+	
+		Errors errors=new BindException(result.getTarget(), area.name()); 
+	
+		for(int i=0;i<list.size();i++) {
+			MetaBase meta=list.get(i);
+			String constraint=map.get(meta.getK());
+			if (constraint==null) continue;
+
+			if (constraint.startsWith(Constraint.Regexp.name()+":")) {
+				//TODO: optimize for speed (pre-compile) if used outside of admin pages
+				String regexp=constraint.substring(Constraint.Regexp.name().length()+1); 
+				Pattern p = Pattern.compile(regexp);
+				Matcher m = p.matcher(meta.getV());
+				boolean b = m.matches();
+				if (!b) {
+					errors.rejectValue(area+"Meta["+i+"].k", area+"."+meta.getK().replace(area+".", "")+".error");  
+				}
+
+			} else if (constraint.equals(Constraint.NotEmpty.name())){
+				if (meta.getV()==null || meta.getV().isEmpty()) {
+					String fieldName=area+"Meta["+i+"].k";
+					String errorMessageKey=area+"."+meta.getK().replace(area+".", "")+".error";
+					errors.rejectValue(fieldName,errorMessageKey );
+				}
+					
+			} else {
+				throw new IllegalStateException("Unknown constraint "+constraint+"|"+meta);
+			}
+		}
+		result.addAllErrors(errors);
+	}
+}
