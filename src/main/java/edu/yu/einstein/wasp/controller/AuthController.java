7bc9473981e694efd296d8573808be7176d28015
Ed Cheng <ed@boylesoftware.com>
2011-08-25 22:01:55 +0000

    drop out SecurityContext

--- src/main/java/edu/yu/einstein/wasp/controller/AuthController.java
+++ src/main/java/edu/yu/einstein/wasp/controller/AuthController.java
@@ -13,8 +13,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.context.SecurityContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
