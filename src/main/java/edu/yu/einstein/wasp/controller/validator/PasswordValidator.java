2f1fe692727cac2844d7f0aef2e8239c4ffdd60f
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2011-08-30 23:09:24 +0000

    minor improvement

--- src/main/java/edu/yu/einstein/wasp/controller/validator/PasswordValidator.java
+++ src/main/java/edu/yu/einstein/wasp/controller/validator/PasswordValidator.java
@@ -1,6 +1,5 @@
 package edu.yu.einstein.wasp.controller.validator;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
@@ -14,11 +13,11 @@ public class PasswordValidator{
     private PasswordService passwordService = new PasswordServiceImpl();
 	
 	public void validate(BindingResult result, String password1, String password2, MetaAttribute.Area area) {
-		String areaName = area.name();
-		Errors errors = new BindException(result.getTarget(), areaName);
-		    
+				    
 	    // validate password
 	    if (! result.hasFieldErrors("password")){
+	    	String areaName = area.name();
+			Errors errors = new BindException(result.getTarget(), areaName);
 	    	if (password1 == null || password1.isEmpty()){
 	    		errors.rejectValue("password", areaName+".password.error", areaName+".password.error (no message has been defined for this property)");
 	    	}
@@ -28,8 +27,8 @@ public class PasswordValidator{
 		    else if (! passwordService.matchPassword(password1, password2) ){
 		    	errors.rejectValue("password", areaName+".password_mismatch.error", areaName+".password_mismatch.error (no message has been defined for this property)");
 		    }
+	    	result.addAllErrors(errors);
 	    }
-	    result.addAllErrors(errors);
 	}
 	
 }
