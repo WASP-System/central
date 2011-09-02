b3a2d9389fe95204c7262fbbf0629b2f2f1ce649
Ed Cheng <ed@boylesoftware.com>
2011-09-02 03:19:15 +0000

    added string implementation

--- src/main/java/edu/yu/einstein/wasp/controller/validator/PasswordValidator.java
+++ src/main/java/edu/yu/einstein/wasp/controller/validator/PasswordValidator.java
@@ -18,25 +18,27 @@ public class PasswordValidator{
 	@Autowired
 	private PasswordService passwordService;
 	
-	
 	public void validate(BindingResult result, String password1, String password2, MetaAttribute.Area area, String passwordFieldName) {
+		validate(result, password1, password2, area.name(), passwordFieldName);
+	}
+	
+	public void validate(BindingResult result, String password1, String password2, String area, String passwordFieldName) {
 				    
 	    // validate password
 	    if (! result.hasFieldErrors(passwordFieldName)){
-	    	String areaName = area.name();
-			Errors errors = new BindException(result.getTarget(), areaName);
+		Errors errors = new BindException(result.getTarget(), area);
 	    	if (password1 == null || password1.isEmpty()){
-	    		errors.rejectValue(passwordFieldName, areaName+".password.error", areaName+".password.error (no message has been defined for this property)");
+	    		errors.rejectValue(passwordFieldName, area +".password.error", area +".password.error (no message has been defined for this property)");
 	    	}
 	    	logger.debug("ANDY: "+password1);
 	    	if (passwordService==null){
 	    		logger.debug("ANDY: password service is null");
 	    	}
 	    	else if (! passwordService.validatePassword(password1) ){ 
-		    	errors.rejectValue(passwordFieldName, areaName+".password_invalid.error", areaName+".password_invalid.error (no message has been defined for this property)");
+		    	errors.rejectValue(passwordFieldName, area +".password_invalid.error", area +".password_invalid.error (no message has been defined for this property)");
 		    }
 		    else if (! passwordService.matchPassword(password1, password2) ){
-		    	errors.rejectValue(passwordFieldName, areaName+".password_mismatch.error", areaName+".password_mismatch.error (no message has been defined for this property)");
+		    	errors.rejectValue(passwordFieldName, area +".password_mismatch.error", area +".password_mismatch.error (no message has been defined for this property)");
 		    }
 	    	result.addAllErrors(errors);
 	    }
