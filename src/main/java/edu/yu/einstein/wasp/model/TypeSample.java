1f57be455443776f323f79afcd8751d98caea806
Ed Cheng <ed@boylesoftware.com>
2011-09-07 22:07:32 +0000

    javadocing models

--- src/main/java/edu/yu/einstein/wasp/model/TypeSample.java
+++ src/main/java/edu/yu/einstein/wasp/model/TypeSample.java
@@ -4,64 +4,186 @@
  * TypeSample.java 
  * @author echeng (table2type.pl)
  *  
- * the TypeSample object
+ * the TypeSample
  *
  *
  */
 
 package edu.yu.einstein.wasp.model;
 
-import org.hibernate.*;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.*;
+
 import org.hibernate.envers.Audited;
 import org.hibernate.envers.NotAudited;
 
-import javax.persistence.*;
-import java.util.*;
+import org.hibernate.validator.constraints.*;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
 
 @Entity
 @Audited
 @Table(name="typesample")
 public class TypeSample extends WaspModel {
-  @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
-  protected int typeSampleId;
-  public void setTypeSampleId (int typeSampleId) {
-    this.typeSampleId = typeSampleId;
-  }
-  public int getTypeSampleId () {
-    return this.typeSampleId;
-  }
+
+	/** 
+	 * typeSampleId
+	 *
+	 */
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	protected int typeSampleId;
+
+	/**
+	 * setTypeSampleId(int typeSampleId)
+	 *
+	 * @param typeSampleId
+	 *
+	 */
+	
+	public void setTypeSampleId (int typeSampleId) {
+		this.typeSampleId = typeSampleId;
+	}
+
+	/**
+	 * getTypeSampleId()
+	 *
+	 * @return typeSampleId
+	 *
+	 */
+	public int getTypeSampleId () {
+		return this.typeSampleId;
+	}
 
 
-  @Column(name="iname")
-  protected String iName;
-  public void setIName (String iName) {
-    this.iName = iName;
-  }
-  public String getIName () {
-    return this.iName;
-  }
 
 
-  @Column(name="name")
-  protected String name;
-  public void setName (String name) {
-    this.name = name;
-  }
-  public String getName () {
-    return this.name;
-  }
+	/** 
+	 * iName
+	 *
+	 */
+	@Column(name="iname")
+	protected String iName;
+
+	/**
+	 * setIName(String iName)
+	 *
+	 * @param iName
+	 *
+	 */
+	
+	public void setIName (String iName) {
+		this.iName = iName;
+	}
+
+	/**
+	 * getIName()
+	 *
+	 * @return iName
+	 *
+	 */
+	public String getIName () {
+		return this.iName;
+	}
 
 
-  @NotAudited
-  @OneToMany
-   @JoinColumn(name="typesampleid", insertable=false, updatable=false)
-  protected List<Sample> sample;
-  public List<Sample> getSample()  {
-    return this.sample;
-  }
-  public void setSample (List<Sample> sample)  {
-    this.sample = sample;
-  }
+
+
+	/** 
+	 * name
+	 *
+	 */
+	@Column(name="name")
+	protected String name;
+
+	/**
+	 * setName(String name)
+	 *
+	 * @param name
+	 *
+	 */
+	
+	public void setName (String name) {
+		this.name = name;
+	}
+
+	/**
+	 * getName()
+	 *
+	 * @return name
+	 *
+	 */
+	public String getName () {
+		return this.name;
+	}
+
+
+
+
+	/** 
+	 * sample
+	 *
+	 */
+	@NotAudited
+	@OneToMany
+	@JoinColumn(name="typesampleid", insertable=false, updatable=false)
+	protected List<Sample> sample;
+
+
+	/** 
+	 * getSample()
+	 *
+	 * @return sample
+	 *
+	 */
+	public List<Sample> getSample() {
+		return this.sample;
+	}
+
+
+	/** 
+	 * setSample
+	 *
+	 * @param sample
+	 *
+	 */
+	public void setSample (List<Sample> sample) {
+		this.sample = sample;
+	}
+
+
+
+	/** 
+	 * sampleDraft
+	 *
+	 */
+	@NotAudited
+	@OneToMany
+	@JoinColumn(name="typesampleid", insertable=false, updatable=false)
+	protected List<SampleDraft> sampleDraft;
+
+
+	/** 
+	 * getSampleDraft()
+	 *
+	 * @return sampleDraft
+	 *
+	 */
+	public List<SampleDraft> getSampleDraft() {
+		return this.sampleDraft;
+	}
+
+
+	/** 
+	 * setSampleDraft
+	 *
+	 * @param sampleDraft
+	 *
+	 */
+	public void setSampleDraft (List<SampleDraft> sampleDraft) {
+		this.sampleDraft = sampleDraft;
+	}
 
 
 
