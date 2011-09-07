045837779a4d4eabeaf17b16deaa0fc66b772b6c
Ed Cheng <ed@boylesoftware.com>
2011-09-07 21:52:07 +0000

    javadocing models

--- src/main/java/edu/yu/einstein/wasp/model/AcctWorkflowcost.java
+++ src/main/java/edu/yu/einstein/wasp/model/AcctWorkflowcost.java
@@ -4,75 +4,184 @@
  * AcctWorkflowcost.java 
  * @author echeng (table2type.pl)
  *  
- * the AcctWorkflowcost object
+ * the AcctWorkflowcost
  *
  *
  */
 
 package edu.yu.einstein.wasp.model;
 
-import org.hibernate.*;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.*;
+
 import org.hibernate.envers.Audited;
 import org.hibernate.envers.NotAudited;
 
-import javax.persistence.*;
-import java.util.*;
+import org.hibernate.validator.constraints.*;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
 
 @Entity
 @Audited
 @Table(name="acct_workflowcost")
 public class AcctWorkflowcost extends WaspModel {
-  @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
-  protected int workflowId;
-  public void setWorkflowId (int workflowId) {
-    this.workflowId = workflowId;
-  }
-  public int getWorkflowId () {
-    return this.workflowId;
-  }
 
+	/** 
+	 * workflowId
+	 *
+	 */
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	protected int workflowId;
 
-  @Column(name="basecost")
-  protected float basecost;
-  public void setBasecost (float basecost) {
-    this.basecost = basecost;
-  }
-  public float getBasecost () {
-    return this.basecost;
-  }
+	/**
+	 * setWorkflowId(int workflowId)
+	 *
+	 * @param workflowId
+	 *
+	 */
+	
+	public void setWorkflowId (int workflowId) {
+		this.workflowId = workflowId;
+	}
 
-
-  @Column(name="lastupdts")
-  protected Date lastUpdTs;
-  public void setLastUpdTs (Date lastUpdTs) {
-    this.lastUpdTs = lastUpdTs;
-  }
-  public Date getLastUpdTs () {
-    return this.lastUpdTs;
-  }
-
-
-  @Column(name="lastupduser")
-  protected int lastUpdUser;
-  public void setLastUpdUser (int lastUpdUser) {
-    this.lastUpdUser = lastUpdUser;
-  }
-  public int getLastUpdUser () {
-    return this.lastUpdUser;
-  }
+	/**
+	 * getWorkflowId()
+	 *
+	 * @return workflowId
+	 *
+	 */
+	public int getWorkflowId () {
+		return this.workflowId;
+	}
 
 
 
-  @NotAudited
-  @ManyToOne
-   @JoinColumn(name="workflowid", insertable=false, updatable=false)
-  protected Job job;
-  public void setJob (Job job) {
-    this.job = job;
-    this.workflowId = job.workflowId;
-  }
-  public Job getJob () {
-    return this.job;
-  }
+
+	/** 
+	 * basecost
+	 *
+	 */
+	@Column(name="basecost")
+	protected float basecost;
+
+	/**
+	 * setBasecost(float basecost)
+	 *
+	 * @param basecost
+	 *
+	 */
+	
+	public void setBasecost (float basecost) {
+		this.basecost = basecost;
+	}
+
+	/**
+	 * getBasecost()
+	 *
+	 * @return basecost
+	 *
+	 */
+	public float getBasecost () {
+		return this.basecost;
+	}
+
+
+
+
+	/** 
+	 * lastUpdTs
+	 *
+	 */
+	@Column(name="lastupdts")
+	protected Date lastUpdTs;
+
+	/**
+	 * setLastUpdTs(Date lastUpdTs)
+	 *
+	 * @param lastUpdTs
+	 *
+	 */
+	
+	public void setLastUpdTs (Date lastUpdTs) {
+		this.lastUpdTs = lastUpdTs;
+	}
+
+	/**
+	 * getLastUpdTs()
+	 *
+	 * @return lastUpdTs
+	 *
+	 */
+	public Date getLastUpdTs () {
+		return this.lastUpdTs;
+	}
+
+
+
+
+	/** 
+	 * lastUpdUser
+	 *
+	 */
+	@Column(name="lastupduser")
+	protected int lastUpdUser;
+
+	/**
+	 * setLastUpdUser(int lastUpdUser)
+	 *
+	 * @param lastUpdUser
+	 *
+	 */
+	
+	public void setLastUpdUser (int lastUpdUser) {
+		this.lastUpdUser = lastUpdUser;
+	}
+
+	/**
+	 * getLastUpdUser()
+	 *
+	 * @return lastUpdUser
+	 *
+	 */
+	public int getLastUpdUser () {
+		return this.lastUpdUser;
+	}
+
+
+
+
+	/**
+	 * job
+	 *
+	 */
+	@NotAudited
+	@ManyToOne
+	@JoinColumn(name="workflowid", insertable=false, updatable=false)
+	protected Job job;
+
+	/**
+	 * setJob (Job job)
+	 *
+	 * @param job
+	 *
+	 */
+	public void setJob (Job job) {
+		this.job = job;
+		this.workflowId = job.workflowId;
+	}
+
+	/**
+	 * getJob ()
+	 *
+	 * @return job
+	 *
+	 */
+	
+	public Job getJob () {
+		return this.job;
+	}
+
 
 }
