627c753a2cb4709d95fb4e50e347728e180ebcab
Ed Cheng <ed@boylesoftware.com>
2011-09-17 19:19:29 +0000

    using table2type, added subtypeid, manually added filedata

--- src/main/java/edu/yu/einstein/wasp/model/SampleDraft.java
+++ src/main/java/edu/yu/einstein/wasp/model/SampleDraft.java
@@ -4,7 +4,7 @@
  * SampleDraft.java 
  * @author echeng (table2type.pl)
  *  
- * the SampleDraft object
+ * the SampleDraft
  *
  *
  */
@@ -14,223 +14,602 @@ package edu.yu.einstein.wasp.model;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import javax.persistence.Transient;
-import javax.validation.constraints.Min;
+import javax.persistence.*;
 
 import org.hibernate.envers.Audited;
 import org.hibernate.envers.NotAudited;
-import org.hibernate.validator.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.*;
+
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
 
 @Entity
 @Audited
 @Table(name="sampledraft")
 public class SampleDraft extends WaspModel {
+
+public static enum Status {
+pending,
+inprocess,
+processed
+}
+
+	/** 
+	 * sampleDraftId
+	 *
+	 */
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	protected int sampleDraftId;
+
+	/**
+	 * setSampleDraftId(int sampleDraftId)
+	 *
+	 * @param sampleDraftId
+	 *
+	 */
 	
-  public static enum Status {
-	  pending,
-	  inprocess,
-	  processed	 
-  }
+	public void setSampleDraftId (int sampleDraftId) {
+		this.sampleDraftId = sampleDraftId;
+	}
+
+	/**
+	 * getSampleDraftId()
+	 *
+	 * @return sampleDraftId
+	 *
+	 */
+	public int getSampleDraftId () {
+		return this.sampleDraftId;
+	}
+
+
+
+
+	/** 
+	 * typeSampleId
+	 *
+	 */
+	@Column(name="typesampleid")
+	protected int typeSampleId;
+
+	/**
+	 * setTypeSampleId(int typeSampleId)
+	 *
+	 * @param typeSampleId
+	 *
+	 */
 	
-  @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
-  protected int sampleDraftId;
-  public void setSampleDraftId (int sampleDraftId) {
-    this.sampleDraftId = sampleDraftId;
-  }
-  public int getSampleDraftId () {
-    return this.sampleDraftId;
-  } 
+	public void setTypeSampleId (int typeSampleId) {
+		this.typeSampleId = typeSampleId;
+	}
 
-
-  @Column(name="typesampleid")  
-  protected Integer typeSampleId;
-  public void setTypeSampleId (Integer typeSampleId) {
-    this.typeSampleId = typeSampleId;
-  }
-  public Integer getTypeSampleId () {
-    return this.typeSampleId;
-  }
-
-
-  @Column(name="labid")
-  protected int labId;
-  public void setLabId (int labId) {
-    this.labId = labId;
-  }
-  public int getLabId () {
-    return this.labId;
-  }
-
-
-  @Column(name="userid")
-  protected int userId;
-  public void setUserId (int userId) {
-    this.userId = userId;
-  }
-  public int getUserId () {
-    return this.userId;
-  }
-
-
-  @Column(name="jobdraftid")
-  protected Integer jobdraftId;
-  public void setJobdraftId (Integer jobdraftId) {
-    this.jobdraftId = jobdraftId;
-  }
-  public Integer getJobdraftId () {
-    return this.jobdraftId;
-  }
-
-
-  @Column(name="name")
-  @NotEmpty
-  protected String name;
-  public void setName (String name) {
-    this.name = name;
-  }
-  public String getName () {
-    return this.name;
-  }
-
-
-  @Column(name="status")  
-  protected String status;
-  public void setStatus (String status) {
-    this.status = status;
-  }
-  public String getStatus () {
-    return this.status;
-  }
-
-
-  @Column(name="lastupdts")
-  protected Date lastUpdTs;
-  public void setLastUpdTs (Date lastUpdTs) {
-    this.lastUpdTs = lastUpdTs;
-  }
-  public Date getLastUpdTs () {
-    return this.lastUpdTs;
-  }
-
-
-  @Column(name="lastupduser")
-  protected int lastUpdUser;
-  public void setLastUpdUser (int lastUpdUser) {
-    this.lastUpdUser = lastUpdUser;
-  }
-  public int getLastUpdUser () {
-    return this.lastUpdUser;
-  }
+	/**
+	 * getTypeSampleId()
+	 *
+	 * @return typeSampleId
+	 *
+	 */
+	public int getTypeSampleId () {
+		return this.typeSampleId;
+	}
 
 
 
-  @NotAudited
-  @ManyToOne
-   @JoinColumn(name="typesampleid", insertable=false, updatable=false)
-  protected TypeSample typeSample;
-  public void setTypeSample (TypeSample typeSample) {
-    this.typeSample = typeSample;
-    this.typeSampleId = typeSample.typeSampleId;
-  }
-  
-  public TypeSample getTypeSample () {
-    return this.typeSample;
-  }
 
-  @NotAudited
-  @ManyToOne
-   @JoinColumn(name="jobdraftid", insertable=false, updatable=false)
-  protected JobDraft jobDraft;
-  public void setJobDraft (JobDraft jobDraft) {
-    this.jobDraft = jobDraft;
-    this.jobdraftId = jobDraft.jobDraftId;
-  }
-  
-  public JobDraft getJobDraft () {
-    return this.jobDraft;
-  }
+	/** 
+	 * subtypeSampleId
+	 *
+	 */
+	@Column(name="subtypesampleid")
+	protected int subtypeSampleId;
 
-  @NotAudited
-  @ManyToOne
-   @JoinColumn(name="labid", insertable=false, updatable=false)
-  protected Lab lab;
-  public void setLab (Lab lab) {
-    this.lab = lab;
-    this.labId = lab.labId;
-  }
-  
-  public Lab getLab () {
-    return this.lab;
-  }
+	/**
+	 * setSubtypeSampleId(int subtypeSampleId)
+	 *
+	 * @param subtypeSampleId
+	 *
+	 */
+	
+	public void setSubtypeSampleId (int subtypeSampleId) {
+		this.subtypeSampleId = subtypeSampleId;
+	}
 
-  @NotAudited
-  @ManyToOne
-   @JoinColumn(name="userid", insertable=false, updatable=false)
-  protected User user;
-  public void setUser (User user) {
-    this.user = user;
-    this.userId = user.UserId;
-  }
-  
-  public User getUser () {
-    return this.user;
-  }
-  @NotAudited
-  @OneToMany
-   @JoinColumn(name="sampledraftid", insertable=false, updatable=false)
-  protected List<SampleDraftMeta> sampleDraftMeta;
-  public List<SampleDraftMeta> getSampleDraftMeta()  {
-    return this.sampleDraftMeta;
-  }
-  public void setSampleDraftMeta (List<SampleDraftMeta> sampleDraftMeta)  {
-    this.sampleDraftMeta = sampleDraftMeta;
-  }
-
-  @Column(name="fileid")
-  private Integer fileId;  
-  public Integer getFileId() {
-	return fileId;
-  }
-  public void setFileId(Integer fileId) {
-	this.fileId = fileId;
-  }
+	/**
+	 * getSubtypeSampleId()
+	 *
+	 * @return subtypeSampleId
+	 *
+	 */
+	public int getSubtypeSampleId () {
+		return this.subtypeSampleId;
+	}
 
 
-  @NotAudited
-  @OneToOne
-   @JoinColumn(name="fileid", insertable=false, updatable=false)
-  protected File file;
-  public void setFile (File file) {
-    this.file = file;
-    this.fileId = file.fileId;
-  }
-  
-  public File getFile () {
-    return this.file;
-  }
-  
-@Transient
- private CommonsMultipartFile fileData;
- public CommonsMultipartFile getFileData()
- {
-   return fileData;
- }
-
- public void setFileData(CommonsMultipartFile fileData)
- {
-   this.fileData = fileData;
- }
 
 
+	/** 
+	 * labId
+	 *
+	 */
+	@Column(name="labid")
+	protected int labId;
+
+	/**
+	 * setLabId(int labId)
+	 *
+	 * @param labId
+	 *
+	 */
+	
+	public void setLabId (int labId) {
+		this.labId = labId;
+	}
+
+	/**
+	 * getLabId()
+	 *
+	 * @return labId
+	 *
+	 */
+	public int getLabId () {
+		return this.labId;
+	}
+
+
+
+
+	/** 
+	 * UserId
+	 *
+	 */
+	@Column(name="userid")
+	protected int UserId;
+
+	/**
+	 * setUserId(int UserId)
+	 *
+	 * @param UserId
+	 *
+	 */
+	
+	public void setUserId (int UserId) {
+		this.UserId = UserId;
+	}
+
+	/**
+	 * getUserId()
+	 *
+	 * @return UserId
+	 *
+	 */
+	public int getUserId () {
+		return this.UserId;
+	}
+
+
+
+
+	/** 
+	 * jobdraftId
+	 *
+	 */
+	@Column(name="jobdraftid")
+	protected Integer jobdraftId;
+
+	/**
+	 * setJobdraftId(Integer jobdraftId)
+	 *
+	 * @param jobdraftId
+	 *
+	 */
+	
+	public void setJobdraftId (Integer jobdraftId) {
+		this.jobdraftId = jobdraftId;
+	}
+
+	/**
+	 * getJobdraftId()
+	 *
+	 * @return jobdraftId
+	 *
+	 */
+	public Integer getJobdraftId () {
+		return this.jobdraftId;
+	}
+
+
+
+
+	/** 
+	 * fileId
+	 *
+	 */
+	@Column(name="fileid")
+	protected Integer fileId;
+
+	/**
+	 * setFileId(Integer fileId)
+	 *
+	 * @param fileId
+	 *
+	 */
+	
+	public void setFileId (Integer fileId) {
+		this.fileId = fileId;
+	}
+
+	/**
+	 * getFileId()
+	 *
+	 * @return fileId
+	 *
+	 */
+	public Integer getFileId () {
+		return this.fileId;
+	}
+
+
+
+
+	/** 
+	 * name
+	 *
+	 */
+	@Column(name="name")
+	@NotEmpty
+	protected String name;
+
+	/**
+	 * setName(String name)
+	 *
+	 * @param name
+	 *
+	 */
+	
+	public void setName (String name) {
+		this.name = name;
+	}
+
+	/**
+	 * getName()
+	 *
+	 * @return name
+	 *
+	 */
+	public String getName () {
+		return this.name;
+	}
+
+
+
+
+	/** 
+	 * status
+	 *
+	 */
+	@Column(name="status")
+	protected String status;
+
+	/**
+	 * setStatus(String status)
+	 *
+	 * @param status
+	 *
+	 */
+	
+	public void setStatus (String status) {
+		this.status = status;
+	}
+
+	/**
+	 * getStatus()
+	 *
+	 * @return status
+	 *
+	 */
+	public String getStatus () {
+		return this.status;
+	}
+
+
+
+
+	/** 
+	 * lastUpdTs
+	 *
+	 */
+	@Column(name="lastupdts")
+	protected Date lastUpdTs;
+
+	/**
+	 * setLastUpdTs(Date lastUpdTs)
+	 *
+	 * @param lastUpdTs
+	 *
+	 */
+	
+	public void setLastUpdTs (Date lastUpdTs) {
+		this.lastUpdTs = lastUpdTs;
+	}
+
+	/**
+	 * getLastUpdTs()
+	 *
+	 * @return lastUpdTs
+	 *
+	 */
+	public Date getLastUpdTs () {
+		return this.lastUpdTs;
+	}
+
+
+
+
+	/** 
+	 * lastUpdUser
+	 *
+	 */
+	@Column(name="lastupduser")
+	protected int lastUpdUser;
+
+	/**
+	 * setLastUpdUser(int lastUpdUser)
+	 *
+	 * @param lastUpdUser
+	 *
+	 */
+	
+	public void setLastUpdUser (int lastUpdUser) {
+		this.lastUpdUser = lastUpdUser;
+	}
+
+	/**
+	 * getLastUpdUser()
+	 *
+	 * @return lastUpdUser
+	 *
+	 */
+	public int getLastUpdUser () {
+		return this.lastUpdUser;
+	}
+
+
+
+
+	/**
+	 * typeSample
+	 *
+	 */
+	@NotAudited
+	@ManyToOne
+	@JoinColumn(name="typesampleid", insertable=false, updatable=false)
+	protected TypeSample typeSample;
+
+	/**
+	 * setTypeSample (TypeSample typeSample)
+	 *
+	 * @param typeSample
+	 *
+	 */
+	public void setTypeSample (TypeSample typeSample) {
+		this.typeSample = typeSample;
+		this.typeSampleId = typeSample.typeSampleId;
+	}
+
+	/**
+	 * getTypeSample ()
+	 *
+	 * @return typeSample
+	 *
+	 */
+	
+	public TypeSample getTypeSample () {
+		return this.typeSample;
+	}
+
+
+	/**
+	 * subtypeSample
+	 *
+	 */
+	@NotAudited
+	@ManyToOne
+	@JoinColumn(name="subtypesampleid", insertable=false, updatable=false)
+	protected SubtypeSample subtypeSample;
+
+	/**
+	 * setSubtypeSample (SubtypeSample subtypeSample)
+	 *
+	 * @param subtypeSample
+	 *
+	 */
+	public void setSubtypeSample (SubtypeSample subtypeSample) {
+		this.subtypeSample = subtypeSample;
+		this.subtypeSampleId = subtypeSample.subtypeSampleId;
+	}
+
+	/**
+	 * getSubtypeSample ()
+	 *
+	 * @return subtypeSample
+	 *
+	 */
+	
+	public SubtypeSample getSubtypeSample () {
+		return this.subtypeSample;
+	}
+
+
+	/**
+	 * jobDraft
+	 *
+	 */
+	@NotAudited
+	@ManyToOne
+	@JoinColumn(name="jobdraftid", insertable=false, updatable=false)
+	protected JobDraft jobDraft;
+
+	/**
+	 * setJobDraft (JobDraft jobDraft)
+	 *
+	 * @param jobDraft
+	 *
+	 */
+	public void setJobDraft (JobDraft jobDraft) {
+		this.jobDraft = jobDraft;
+		this.jobdraftId = jobDraft.jobDraftId;
+	}
+
+	/**
+	 * getJobDraft ()
+	 *
+	 * @return jobDraft
+	 *
+	 */
+	
+	public JobDraft getJobDraft () {
+		return this.jobDraft;
+	}
+
+
+	/**
+	 * lab
+	 *
+	 */
+	@NotAudited
+	@ManyToOne
+	@JoinColumn(name="labid", insertable=false, updatable=false)
+	protected Lab lab;
+
+	/**
+	 * setLab (Lab lab)
+	 *
+	 * @param lab
+	 *
+	 */
+	public void setLab (Lab lab) {
+		this.lab = lab;
+		this.labId = lab.labId;
+	}
+
+	/**
+	 * getLab ()
+	 *
+	 * @return lab
+	 *
+	 */
+	
+	public Lab getLab () {
+		return this.lab;
+	}
+
+
+	/**
+	 * user
+	 *
+	 */
+	@NotAudited
+	@ManyToOne
+	@JoinColumn(name="userid", insertable=false, updatable=false)
+	protected User user;
+
+	/**
+	 * setUser (User user)
+	 *
+	 * @param user
+	 *
+	 */
+	public void setUser (User user) {
+		this.user = user;
+		this.UserId = user.UserId;
+	}
+
+	/**
+	 * getUser ()
+	 *
+	 * @return user
+	 *
+	 */
+	
+	public User getUser () {
+		return this.user;
+	}
+
+
+	/**
+	 * file
+	 *
+	 */
+	@NotAudited
+	@ManyToOne
+	@JoinColumn(name="fileid", insertable=false, updatable=false)
+	protected File file;
+
+	/**
+	 * setFile (File file)
+	 *
+	 * @param file
+	 *
+	 */
+	public void setFile (File file) {
+		this.file = file;
+		this.fileId = file.fileId;
+	}
+
+	/**
+	 * getFile ()
+	 *
+	 * @return file
+	 *
+	 */
+	
+	public File getFile () {
+		return this.file;
+	}
+
+
+	/** 
+	 * sampleDraftMeta
+	 *
+	 */
+	@NotAudited
+	@OneToMany
+	@JoinColumn(name="sampledraftid", insertable=false, updatable=false)
+	protected List<SampleDraftMeta> sampleDraftMeta;
+
+
+	/** 
+	 * getSampleDraftMeta()
+	 *
+	 * @return sampleDraftMeta
+	 *
+	 */
+	public List<SampleDraftMeta> getSampleDraftMeta() {
+		return this.sampleDraftMeta;
+	}
+
+
+	/** 
+	 * setSampleDraftMeta
+	 *
+	 * @param sampleDraftMeta
+	 *
+	 */
+	public void setSampleDraftMeta (List<SampleDraftMeta> sampleDraftMeta) {
+		this.sampleDraftMeta = sampleDraftMeta;
+	}
+
+
+
+	@Transient
+	private CommonsMultipartFile fileData;
+	public CommonsMultipartFile getFileData() {
+		return fileData;
+	}
+
+	public void setFileData(CommonsMultipartFile fileData) {
+		this.fileData = fileData;
+	}
 }
