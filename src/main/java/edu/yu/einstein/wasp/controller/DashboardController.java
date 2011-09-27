f2cd480bb07aa3dd353663ceb4cb83437e22c29f
Ed Cheng <ed@boylesoftware.com>
2011-09-27 20:16:40 +0000

    department sic fix

--- src/main/java/edu/yu/einstein/wasp/controller/DashboardController.java
+++ src/main/java/edu/yu/einstein/wasp/controller/DashboardController.java
@@ -18,55 +18,59 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Transactional
 public class DashboardController extends WaspController {
 
-  @Autowired
-  private DepartmentService departmentService;
+	@Autowired
+	private DepartmentService departmentService;
 
-  @Autowired
-  private LabService labService;
+	@Autowired
+	private LabService labService;
 
-  @Autowired
-  private JobService jobService;
+	@Autowired
+	private JobService jobService;
 
-  @Autowired
-  private JobDraftService jobDraftService;
+	@Autowired
+	private JobDraftService jobDraftService;
 
-  public static enum DashboardEntityRolename {du, lu, jv, jd};
+	// list of baserolenames (du-department admin, lu- labuser ...)
+	//   see role table
+  // higher level roles such as 'lm' or 'js' are used on the view
+	public static enum DashboardEntityRolename {du, lu, jv, jd};
 
-  @RequestMapping("/dashboard")
-  public String list(ModelMap m) {
-    List<Department> departmentList= new ArrayList<Department>();
-    List<Lab> labList = new ArrayList<Lab>();
-    List<Job> jobList = new ArrayList<Job>();
-    List<JobDraft> jobDraftList = new ArrayList<JobDraft>();
+	@RequestMapping("/dashboard")
+	public String list(ModelMap m) {
+		List<Department> departmentList= new ArrayList<Department>();
+		List<Lab> labList = new ArrayList<Lab>();
+		List<Job> jobList = new ArrayList<Job>();
+		List<JobDraft> jobDraftList = new ArrayList<JobDraft>();
 
-    for (String role: getRoles()) {
-      String[] splitRole = role.split("-");
-      if (splitRole.length != 2) { continue; }
-      if (splitRole[1].equals("*")) { continue; }
-    
-      DashboardEntityRolename entityRolename; 
-      int roleObjectId = 0;
+		for (String role: getRoles()) {
+			String[] splitRole = role.split("-");
+			if (splitRole.length != 2) { continue; }
+			if (splitRole[1].equals("*")) { continue; }
+		
+			DashboardEntityRolename entityRolename; 
+			int roleObjectId = 0;
 
-      try { 
-        entityRolename = DashboardEntityRolename.valueOf(splitRole[0]);
-        roleObjectId = Integer.parseInt(splitRole[1]);
-      } catch (Exception e)  {
-        continue;
-      }
+			try { 
+				entityRolename = DashboardEntityRolename.valueOf(splitRole[0]);
+				roleObjectId = Integer.parseInt(splitRole[1]);
+			} catch (Exception e)	{
+				continue;
+			}
 
-      switch (entityRolename) {
-        case du: departmentList.add(departmentService.getDepartmentByDepartmentId(roleObjectId)); break;
-        case lu: labList.add(labService.getLabByLabId(roleObjectId)); break;
-        case jv: jobList.add(jobService.getJobByJobId(roleObjectId)); break;
-        case jd: jobDraftList.add(jobDraftService.getJobDraftByJobDraftId(roleObjectId)); break;
-      }
-    }
+			// adds the role ojbect to the proper bucket
+			switch (entityRolename) {
+				case du: departmentList.add(departmentService.getDepartmentByDepartmentId(roleObjectId)); break;
+				case lu: labList.add(labService.getLabByLabId(roleObjectId)); break;
+				case jv: jobList.add(jobService.getJobByJobId(roleObjectId)); break;
+				case jd: jobDraftList.add(jobDraftService.getJobDraftByJobDraftId(roleObjectId)); break;
+			}
+		}
 
-    m.addAttribute("departements", departmentList);
-    m.addAttribute("labs", labList);
-    m.addAttribute("jobs", jobList);
-    m.addAttribute("jobdrafts", jobDraftList);
+		m.addAttribute("departments", departmentList);
+		m.addAttribute("labs", labList);
+		m.addAttribute("jobs", jobList);
+		m.addAttribute("jobdrafts", jobDraftList);
 
-    return "dashboard";
-  }
+		return "dashboard";
+	}
 }
