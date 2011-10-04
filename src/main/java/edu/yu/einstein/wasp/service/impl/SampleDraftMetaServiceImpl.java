e571da06ff443e6b6c0a7d41bc103e07acf940c1
Ed Cheng <ed@boylesoftware.com>
2011-10-04 20:26:52 +0000

    revert

--- src/main/java/edu/yu/einstein/wasp/service/impl/SampleDraftMetaServiceImpl.java
+++ src/main/java/edu/yu/einstein/wasp/service/impl/SampleDraftMetaServiceImpl.java
@@ -1,62 +1,43 @@
 
 /**
  *
- * SampleDraftMetaServiceImpl.java 
+ * SampleDraftMetaService.java 
  * @author echeng (table2type.pl)
  *  
- * the SampleDraftMetaService Implmentation 
+ * the SampleDraftMetaService object
  *
  *
  **/
 
 package edu.yu.einstein.wasp.service.impl;
 
-import edu.yu.einstein.wasp.service.SampleDraftMetaService;
-import edu.yu.einstein.wasp.dao.SampleDraftMetaDao;
-import edu.yu.einstein.wasp.dao.WaspDao;
-import edu.yu.einstein.wasp.model.SampleDraftMeta;
-
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PostFilter;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Service;
+
+import edu.yu.einstein.wasp.dao.SampleDraftMetaDao;
+import edu.yu.einstein.wasp.model.SampleDraftMeta;
+import edu.yu.einstein.wasp.model.SubtypeSample;
+import edu.yu.einstein.wasp.service.SampleDraftMetaService;
 
 @Service
 public class SampleDraftMetaServiceImpl extends WaspServiceImpl<SampleDraftMeta> implements SampleDraftMetaService {
 
-	/**
-	 * sampleDraftMetaDao;
-	 *
-	 */
-	private SampleDraftMetaDao sampleDraftMetaDao;
+  private SampleDraftMetaDao sampleDraftMetaDao;
+  @Autowired
+  public void setSampleDraftMetaDao(SampleDraftMetaDao sampleDraftMetaDao) {
+    this.sampleDraftMetaDao = sampleDraftMetaDao;
+    this.setWaspDao(sampleDraftMetaDao);
+  }
+  public SampleDraftMetaDao getSampleDraftMetaDao() {
+    return this.sampleDraftMetaDao;
+  }
 
-	/**
-	 * setSampleDraftMetaDao(SampleDraftMetaDao sampleDraftMetaDao)
-	 *
-	 * @param sampleDraftMetaDao
-	 *
-	 */
-	@Autowired
-	public void setSampleDraftMetaDao(SampleDraftMetaDao sampleDraftMetaDao) {
-		this.sampleDraftMetaDao = sampleDraftMetaDao;
-		this.setWaspDao(sampleDraftMetaDao);
-	}
+  // **
 
-	/**
-	 * getSampleDraftMetaDao();
-	 *
-	 * @return sampleDraftMetaDao
-	 *
-	 */
-	public SampleDraftMetaDao getSampleDraftMetaDao() {
-		return this.sampleDraftMetaDao;
-	}
-
-
+  
   public SampleDraftMeta getSampleDraftMetaBySampleDraftMetaId (final int sampleDraftMetaId) {
     return this.getSampleDraftMetaDao().getSampleDraftMetaBySampleDraftMetaId(sampleDraftMetaId);
   }
@@ -65,18 +46,14 @@ public class SampleDraftMetaServiceImpl extends WaspServiceImpl<SampleDraftMeta>
     return this.getSampleDraftMetaDao().getSampleDraftMetaByKSampledraftId(k, sampledraftId);
   }
 
-  public void updateBySampledraftId (final String area, final int sampledraftId, final List<SampleDraftMeta> metaList) {
-    this.getSampleDraftMetaDao().updateBySampledraftId(area, sampledraftId, metaList); 
-  }
-
   public void updateBySampledraftId (final int sampledraftId, final List<SampleDraftMeta> metaList) {
     this.getSampleDraftMetaDao().updateBySampledraftId(sampledraftId, metaList); 
   }
-
+  
   public Map<SubtypeSample,List<SampleDraftMeta>> getAllowableMetaFields(int workflowId) {
-    return this.getSampleDraftMetaDao().getAllowableMetaFields(workflowId);
+	  return this.getSampleDraftMetaDao().getAllowableMetaFields(workflowId);	  
   }
-
-
+  
+  
 }
 
