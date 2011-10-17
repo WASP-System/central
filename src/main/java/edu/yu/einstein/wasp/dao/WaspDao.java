5f2a31dae2fb65aca4fac54b949d79d4df7c7dad
Robert Dubin <robert.dubin@einstein.yu.edu>
2011-10-17 18:08:08 +0000

    <empty commit message>

--- src/main/java/edu/yu/einstein/wasp/dao/WaspDao.java
+++ src/main/java/edu/yu/einstein/wasp/dao/WaspDao.java
@@ -60,6 +60,62 @@ public interface WaspDao<E> {
  public Integer removeAll();
 
  public List findByMap(Map m);
+ 
+ /**
+  * Robert Dubin; 17 Oct 2011
+  * Generates and executes simple SQL statement that includes
+  * 	1. single (optional) DISTINCT   
+  * 	2. single (optional) ORDER BY 
+  * 	3. single (optional) direction for ORDER BY (used only if an ORDER BY paramater is provided) 
+  * 
+  * 	@param final String distinctColumnName for the single (optional) DISTINCT attribute
+  * 	@param final String orderByColumnName for the single (optional) ORDER BY attribute
+  * 	@param final String direction used in conjunction with the ORDER BY statement (if orderByColumnName is not empty)
+  * 	@return List of objects returned by the database that fulfill the query 
+  * 
+  * 	example use: List<Department> departmentList = this.getDepartmentService().findDistinctOrderBy("name", "name", "ASC");
+  */
+ public List findDistinctOrderBy(final String distinctColumnName, final String orderByColumnName, final String direction);
+ 
+ /**
+  * Robert Dubin; 17 Oct 2011
+  * Generates and executes simple SQL statement that includes
+  *	1. single (optional) ORDER BY 
+  * 	2. single (optional) direction for ORDER BY (used only if an ORDER BY paramater is provided) 
+  * 
+  * 	@param final String orderByColumnName for the single (optional) ORDER BY attribute
+  * 	@param final String direction used in conjunction with the ORDER BY statement (if orderByColumnName is not empty)
+  * 	@return List of objects returned by the database that fulfill the query 
+  * 
+  *    example use: List<Department> departmentList = this.getDepartmentService().findAllOrderBy("name", "ASC");
+  */
+ public List findAllOrderBy(final String orderByColumnName, final String direction);
+ /**
+  * Robert Dubin; 17 Oct 2011
+  * Generates and executes SQL statement that includes
+  * 	1. multiple (optional) WHERE attributes 
+  * 	2. multiple (optional) DISTINCT attributes 
+  * 	3. multiple (optional) ORDER BY attributes 
+  * 	4. single (optional) direction (DESC or ASC) for ORDER BY  (if there is at least one ORDER BY attribute)
+  * 
+  * 	@param final Map m for the multiple (optional) WHERE attributes
+  * 	@param final List<String> distinctColumnNames for the multiple (optional) DISTINCT attributes
+  * 	@param final List<String> orderByColumnNames for the multiple (optional) ORDER BY attributes
+  * 	@param final String direction (optinal) used in conjunction with the ORDER BY statement (if at least one ORDER BY attribute provided) 
+  * 	@return List of objects returned by the database that fulfill the query 
+  * 
+  * example use:
+  * 	Map whereConstraints = new HashMap();
+  *	whereConstraints.put("isInternal", 1);
+  *	whereConstraints.put("isActive", 1);
+  *	List<String> distinctConstraints = new ArrayList<String>();
+  *	distinctConstraints.add("name");	  
+  *	List<String> orderConstraints = new ArrayList<String>();
+  *	orderConstraints.add("name");	  
+  *	String direction = "ASC";
+  *	List<Department> departmentList = this.getDepartmentService().findByMapDistinctOrderBy(whereConstraints, distinctConstraints, orderConstraints, direction);
+  */
+ public List findByMapDistinctOrderBy(final Map m, final List<String> distinctColumnNames, final List<String> orderByColumnNames, final String direction);
 
 }
 
