d3996f419e292b510b358bead431c29049164b3c
Ed Cheng <ed@boylesoftware.com>
2011-10-19 03:58:36 +0000

    initial ci

---
+++ src/main/java/edu/yu/einstein/wasp/controller/SampleDnaToLibraryController.java
@@ -0,0 +1,209 @@
+package edu.yu.einstein.wasp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.transaction.annotation.*;
+
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+
+import java.util.Date;
+import java.util.List;
+
+import edu.yu.einstein.wasp.service.SampleService;
+import edu.yu.einstein.wasp.service.SampleMetaService;
+import edu.yu.einstein.wasp.model.*;
+
+@Controller
+@Transactional
+@RequestMapping("/sampleDnaToLibrary")
+public class SampleDnaToLibraryController extends WaspController {
+
+  private SampleService sampleService;
+  @Autowired
+  public void setSampleService(SampleService sampleService) {
+    this.sampleService = sampleService;
+  }
+  public SampleService getSampleService() {
+    return this.sampleService;
+  }
+
+  @Autowired
+  private SampleMetaService sampleMetaService;
+
+  private final MetaHelper getMetaHelper() {
+    return new MetaHelper("sample", SampleMeta.class, request.getSession());
+  }
+
+
+
+  final public String defaultPageFlow = "/sampleDnaToLibrary/detail/{n};/sampleDnaToLibrary/addLibraryMeta/{n};/sampleDnaToLibrary/verify/{n}";
+
+  public String nextPage(Sample sample) {
+     String pageFlow = this.defaultPageFlow;
+/*
+     try {
+       List<WorkflowMeta> wfmList = sample.getWorkflow().getWorkflowMeta();
+       for (WorkflowMeta wfm : wfmList) {
+         if (wfm.getK().equals("workflow.submitpageflow")) {
+           pageFlow = wfm.getV();
+           break;
+         }
+       }
+     } catch (Exception e) {
+     }
+*/
+
+
+    String context = request.getContextPath();
+    String uri = request.getRequestURI();
+    String path = request.getServletPath();
+
+    // strips context, lead slash ("/"), spring mapping
+    String currentMapping = uri.replaceFirst(context, "").replaceFirst("\\.do.*$", "");
+
+
+    String pageFlowArray[] = pageFlow.split(";");
+
+    int found = -1;
+    for (int i=0; i < pageFlowArray.length -1; i++) {
+      String page = pageFlowArray[i];
+      page = page.replaceAll("\\{n\\}", ""+sample.getSampleId());
+
+      if (currentMapping.equals(page)) {
+        found = i;
+        break;
+      }
+    }
+
+    String targetPage = pageFlowArray[found+1] + ".do";
+
+    targetPage = targetPage.replaceAll("\\{n\\}", ""+sample.getSampleId());
+
+    return "redirect:" + targetPage;
+  }
+
+
+  @RequestMapping(value="/detail/{sampleId}", method=RequestMethod.GET)
+  public String detail(@PathVariable("sampleId") Integer sampleId, ModelMap m) {
+    Sample sample = sampleService.getSampleBySampleId(sampleId); 
+
+    MetaHelper metaHelper = getMetaHelper();
+    List<SampleMeta> coreSampleMeta = metaHelper.syncWithMaster(sample.getSampleMeta());
+
+    metaHelper.setArea("sampleDnaToLibrary");
+
+    List<SampleMeta> sampleDnaLToLibrarySampleMeta = metaHelper.syncWithMaster(sample.getSampleMeta());
+    
+    m.put("sample", sample); 
+    m.put("sampleId", sampleId); 
+    m.put("coreMeta", coreSampleMeta); 
+    m.put("sampleDnaToLibSampleMeta", sampleDnaLToLibrarySampleMeta); 
+
+    m.put("area", metaHelper.getArea());
+    m.put("parentarea", metaHelper.getParentArea());
+  
+    return "sampleDnaToLibrary/detail";
+  }
+
+
+  @RequestMapping(value="/addLibraryMeta/{sampleId}", method=RequestMethod.GET)
+  public String addLibraryMetaForm(@PathVariable("sampleId") Integer sampleId, ModelMap m) {
+    Sample sample = sampleService.getSampleBySampleId(sampleId); 
+
+    MetaHelper metaHelper = getMetaHelper();
+    List<SampleMeta> coreSampleMeta = metaHelper.syncWithMaster(sample.getSampleMeta());
+
+    metaHelper.setArea("sampleDnaToLibraryAddLibraryMeta");
+
+    List<SampleMeta> sampleDnaLToLibrarySampleMeta = metaHelper.syncWithMaster(sample.getSampleMeta());
+    
+    m.put("sample", sample); 
+    m.put("sampleId", sampleId); 
+    m.put("coreMeta", coreSampleMeta); 
+    m.put("sampleDnaToLibSampleMeta", sampleDnaLToLibrarySampleMeta); 
+
+    m.put("area", metaHelper.getArea());
+    m.put("parentarea", metaHelper.getParentArea());
+  
+    return "sampleDnaToLibrary/addLibraryMeta";
+  }
+
+  @RequestMapping(value="/detail/{sampleId}", method=RequestMethod.POST)
+  public String modifyDetail (
+     @PathVariable Integer sampleId,
+     @Valid Sample sampleForm,
+     BindingResult result,
+     SessionStatus status,
+     ModelMap m) {
+
+     return appendMeta(sampleId, sampleForm, result, status, m, "sampleDnaToLibrary", "sampleDnaToLibrary/detail");
+  }
+
+  @RequestMapping(value="/addLibraryMeta/{sampleId}", method=RequestMethod.POST)
+  public String modifyLibraryMeta (
+     @PathVariable Integer sampleId,
+     @Valid Sample sampleForm,
+     BindingResult result,
+     SessionStatus status,
+     ModelMap m) {
+
+     return appendMeta(sampleId, sampleForm, result, status, m, "sampleDnaToLibraryAddLibraryMeta",  "sampleDnaToLibrary/addLibraryMeta");
+  }
+
+
+  protected String appendMeta(
+       @PathVariable Integer sampleId,
+       @Valid Sample sampleForm,
+       BindingResult result,
+       SessionStatus status,
+       ModelMap m, 
+       String area,
+       String returnPageDef
+     ) {
+     Sample sample = sampleService.getSampleBySampleId(sampleId);
+
+     MetaHelper metaHelper = getMetaHelper();
+     metaHelper.setArea(area);
+
+     List<SampleMeta> sampleMetaList = metaHelper.getFromRequest(request, SampleMeta.class);
+
+     sampleForm.setSampleMeta(sampleMetaList);
+     metaHelper.validate(sampleMetaList, result);
+
+     if (result.hasErrors()) {
+        waspMessage("hello.error");
+
+        metaHelper = getMetaHelper();
+        List<SampleMeta> coreSampleMeta = metaHelper.syncWithMaster(sample.getSampleMeta());
+
+        metaHelper.setArea(area);
+    
+        List<SampleMeta> sampleDnaLToLibrarySampleMeta = sampleMetaList;
+    
+        m.put("sample", sample); 
+        m.put("sampleId", sampleId); 
+        m.put("coreMeta", coreSampleMeta); 
+        m.put("sampleDnaToLibSampleMeta", sampleDnaLToLibrarySampleMeta); 
+
+        m.put("area", metaHelper.getArea());
+        m.put("parentarea", metaHelper.getParentArea());
+  
+        return returnPageDef;
+     }
+ 
+     sampleMetaService.updateBySampleId(metaHelper.getArea(), sampleId, sampleMetaList);
+
+     return nextPage(sample);
+  } 
+}
