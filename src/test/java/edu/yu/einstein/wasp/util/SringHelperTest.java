ea8fc2f1c86d005c496a1b3ee6ed9bc5b7b66d88
Natalia Volvova <natalia.volnova@einstein.yu.edu>
2011-10-19 21:02:11 +0000

    Tests method toCapFirstLetter(String sParam)

---
+++ src/test/java/edu/yu/einstein/wasp/util/SringHelperTest.java
@@ -0,0 +1,40 @@
+package edu.yu.einstein.wasp.util;
+
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SringHelperTest {
+	
+
+	StringHelper stringHelper;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		stringHelper = new StringHelper(); 
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		String expected = new String("Test");
+		assertEquals("",expected, stringHelper.toCapFirstLetter("test"));
+		//fail("Not yet implemented");
+	}
+}
