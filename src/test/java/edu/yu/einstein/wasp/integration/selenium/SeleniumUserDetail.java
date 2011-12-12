61e9278572d9ef6ef9fee05722aba2c99b3672f3
Natalia Volvova <natalia.volnova@einstein.yu.edu>
2011-12-12 15:47:37 +0000

    <empty commit message>

--- src/test/java/edu/yu/einstein/wasp/integration/selenium/SeleniumUserDetail.java
+++ src/test/java/edu/yu/einstein/wasp/integration/selenium/SeleniumUserDetail.java
@@ -44,6 +44,7 @@ public class SeleniumUserDetail extends SeleniumBaseTest {
 	   * @param sApprovedUrl
 	   * @throws Exception
 	   */
+	  
 	  @Test (groups = "integration-tests",  dataProvider = "DP1")
 	  public void navigateMyProfile(String sUserName, String sUserPass) throws Exception {   
 			
@@ -59,34 +60,6 @@ public class SeleniumUserDetail extends SeleniumBaseTest {
 		  driver.findElement(By.xpath("//a[contains(@href,'/wasp/user/me_rw.do')]")).click();
 	      
 		  Assert.assertEquals(driver.getCurrentUrl(), "http://localhost:8080/wasp/user/me_rw.do");
-	  }
-	  
-	  @Test (groups = "integration-tests",  dataProvider = "DP2")
-	  public void createNewLabRequest(String sUserName, String sUserPass,String sLab, String sPhone, String sAddress, String sPhone2, String sUrl, String successUrl) throws Exception {   
-			
-		  SeleniumHelper.login(sUserName, sUserPass, driver);	 
-		  driver.get("http://localhost:8080/wasp/dashboard.do");
-		  Assert.assertTrue(driver.findElements(By.xpath("//a[contains(@href,'/wasp/lab/newrequest.do')]")).size() != 0, "Cannot locate 'REQUEST ACCESS TO LAB' link.");
-		  driver.findElement(By.xpath("//a[contains(@href,'/wasp/lab/newrequest.do')]")).click();
-		  Assert.assertEquals(driver.getCurrentUrl(),sUrl);
-		  
-		  Assert.assertTrue(driver.findElements(By.id("name")).size() != 0, "Cannot locate 'Lab Name' input text box");
-		  driver.findElement(By.id("name")).sendKeys(sLab);
-	  
-		  Assert.assertTrue(driver.findElements(By.id("phone")).size() != 0, "Cannot locate 'Phone' input text box");
-		  driver.findElement(By.id("phone")).sendKeys(sPhone);
-		  
-		  Assert.assertTrue(driver.findElements(By.id("billing_address")).size() != 0, "Cannot locate 'Address' input text box");
-		  driver.findElement(By.id("billing_address")).sendKeys(sAddress);
-		  
-		  Assert.assertTrue(driver.findElements(By.id("billing_phone")).size() != 0, "Cannot locate 'Address' input text box");
-		  driver.findElement(By.id("billing_phone")).sendKeys(sPhone2);
-		  
-		  Assert.assertTrue(driver.findElements(By.xpath("//input[@value='Submit']")).size() != 0, "Cannot locate 'Submit' button.");
-		  driver.findElement(By.xpath("//input[@value='Submit']")).click();
-		  
-		  Assert.assertEquals(driver.getCurrentUrl(), successUrl);
-	      
 	  }
 
 	  @AfterClass
