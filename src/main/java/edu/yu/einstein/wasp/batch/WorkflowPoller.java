7a08163ff5fef43d4daca5e614099d7e66b20846
Ed Cheng <ed@boylesoftware.com>
2011-12-12 05:58:31 +0000

    initial ci

---
+++ src/main/java/edu/yu/einstein/wasp/batch/WorkflowPoller.java
@@ -0,0 +1,71 @@
+package edu.yu.einstein.wasp.batch;
+
+
+import edu.yu.einstein.wasp.model.*;
+import edu.yu.einstein.wasp.service.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList; 
+import java.util.List; 
+import java.util.Map; 
+import java.util.HashMap; 
+
+/**
+ * Workflow Poller
+ * polls job db for task/workflowid combos that are not final
+ *
+ */
+
+@Service
+public class WorkflowPoller {
+
+	@Autowired
+	StateService stateService;
+
+	@Autowired
+	TaskService taskService;
+
+	@Autowired
+	WorkflowService workflowService;
+ 
+	protected String workflowIName; 
+	public void setWorkflowIName(String s) {
+		this.workflowIName = s;
+	}
+
+	protected String taskIName; 
+	public void setTaskIName(String s) {
+		this.taskIName = s;
+	}
+
+ 
+	@Transactional
+	public List<State> getStates() {
+		Workflow workflow = workflowService.getWorkflowByIName(workflowIName); 
+		Task task = taskService.getTaskByIName(taskIName); 
+
+		List<State> rt = new ArrayList();
+
+		List<State> allStates = task.getState();
+
+
+		for (State state: allStates) {
+			if (state.getStatus().equals("FINAL")) { continue; }
+
+			// should be the same so just get first, 
+			// TODO check for npe
+			if (state.getStatejob().get(0).getJob().getWorkflowId() != workflow.getWorkflowId()) {
+				continue;
+			}
+
+
+			rt.add(state);
+		}
+
+		return rt; 
+	}
+
+}
