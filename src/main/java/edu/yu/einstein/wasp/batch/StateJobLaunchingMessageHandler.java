0bb7dc60e949231c3fba5ea9fa101e0ec31ce19b
Ed Cheng <ed@boylesoftware.com>
2011-12-12 06:49:42 +0000

    jobreposistory

--- src/main/java/edu/yu/einstein/wasp/batch/StateJobLaunchingMessageHandler.java
+++ src/main/java/edu/yu/einstein/wasp/batch/StateJobLaunchingMessageHandler.java
@@ -11,6 +11,7 @@ import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.configuration.JobRegistry;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.NoSuchJobException;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
@@ -21,6 +22,7 @@ public class StateJobLaunchingMessageHandler {
 
 	private final JobRegistry jobRegistry;
 	private final JobLauncher jobLauncher;
+	private final JobRepository jobRepository;
 
 	private final String jobName;
 
@@ -29,32 +31,40 @@ public class StateJobLaunchingMessageHandler {
 	 * 
 	 * @param jobRegistry
 	 * @param jobLauncher
+	 * @param jobRepository
 	 * @param jobName
 	 */
-	public StateJobLaunchingMessageHandler(JobRegistry jobRegistry, JobLauncher jobLauncher, String jobName) {
+	public StateJobLaunchingMessageHandler(JobRegistry jobRegistry, JobLauncher jobLauncher, JobRepository jobRepository, String jobName) {
 		super();
 		this.jobRegistry = jobRegistry;
 		this.jobLauncher = jobLauncher;
+		this.jobRepository = jobRepository;
 		this.jobName = jobName;
 	}
 
 	public JobExecution launch(State state) throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException,
 			JobParametersInvalidException, NoSuchJobException {
 
-		if (state == null)  { return null; }
+		if (state == null)	{ return null; }
 		
 		Job job = jobRegistry.getJob(this.jobName);
 		JobParametersBuilder builder = new JobParametersBuilder();
 		builder.addString("state", ""+ state.getStateId());
 		
-		JobExecution jobToken=null;
 		try {
-			jobToken=jobLauncher.run(job, builder.toJobParameters());
+			JobExecution jobToken=null;
+
+			if (jobRepository.isJobInstanceExists(this.jobName, builder.toJobParameters())) {
+				jobToken = jobRepository.getLastJobExecution(this.jobName, builder.toJobParameters());
+			} else {
+				jobToken = jobLauncher.run(job, builder.toJobParameters());
+			}
+
+			return jobToken;
 		} catch (JobInstanceAlreadyCompleteException e) {
 			
 		}
 		
-		return jobToken;
-		
+	  return null;	
 	}
 }
