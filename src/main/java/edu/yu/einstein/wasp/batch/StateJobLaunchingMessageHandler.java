0bb434b8408530418e76366e35ec1083802512c5
Ed Cheng <ed@boylesoftware.com>
2011-12-12 17:41:36 +0000

    passing job operator

--- src/main/java/edu/yu/einstein/wasp/batch/StateJobLaunchingMessageHandler.java
+++ src/main/java/edu/yu/einstein/wasp/batch/StateJobLaunchingMessageHandler.java
@@ -12,17 +12,25 @@ import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.launch.JobOperator;
+
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.NoSuchJobException;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.batch.core.launch.NoSuchJobExecutionException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 public class StateJobLaunchingMessageHandler {
+  private final Log logger = LogFactory.getLog(getClass());
 
 	private final JobRegistry jobRegistry;
 	private final JobLauncher jobLauncher;
 	private final JobRepository jobRepository;
+	private final JobOperator jobOperator;
 
 	private final String jobName;
 
@@ -32,18 +40,20 @@ public class StateJobLaunchingMessageHandler {
 	 * @param jobRegistry
 	 * @param jobLauncher
 	 * @param jobRepository
+	 * @param jobOperator
 	 * @param jobName
 	 */
-	public StateJobLaunchingMessageHandler(JobRegistry jobRegistry, JobLauncher jobLauncher, JobRepository jobRepository, String jobName) {
+	public StateJobLaunchingMessageHandler(JobRegistry jobRegistry, JobLauncher jobLauncher, JobRepository jobRepository, JobOperator jobOperator, String jobName) {
 		super();
 		this.jobRegistry = jobRegistry;
 		this.jobLauncher = jobLauncher;
 		this.jobRepository = jobRepository;
+		this.jobOperator = jobOperator;
 		this.jobName = jobName;
 	}
 
 	public JobExecution launch(State state) throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException,
-			JobParametersInvalidException, NoSuchJobException {
+			JobParametersInvalidException, NoSuchJobException, NoSuchJobExecutionException, org.springframework.batch.core.launch.JobExecutionNotRunningException {
 
 		if (state == null)	{ return null; }
 		
@@ -55,7 +65,13 @@ public class StateJobLaunchingMessageHandler {
 			JobExecution jobToken=null;
 
 			if (jobRepository.isJobInstanceExists(this.jobName, builder.toJobParameters())) {
+
 				jobToken = jobRepository.getLastJobExecution(this.jobName, builder.toJobParameters());
+
+				if (jobToken.isRunning()) {
+					jobOperator.restart(jobToken.getJobId());	
+				}
+
 			} else {
 				jobToken = jobLauncher.run(job, builder.toJobParameters());
 			}
