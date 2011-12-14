7946100047f679bd772904f75402f7e91fa27a32
Ed Cheng <ed@boylesoftware.com>
2011-12-14 07:58:19 +0000

    added startts

--- src/main/java/edu/yu/einstein/wasp/batch/CreateSampleReceiveProcessor.java
+++ src/main/java/edu/yu/einstein/wasp/batch/CreateSampleReceiveProcessor.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Date;
 
 /**
  * Created Sample Receive Processor
@@ -60,6 +61,7 @@ public class CreateSampleReceiveProcessor implements ItemProcessor {
 				newState.setStatus(targetStatus);
 				newState.setTaskId(t.getTaskId());
 				newState.setName(t.getName());
+				newState.setStartts(new Date());
 				State newStateDb = stateService.save(newState);
 
 				Statejob newStateJob = new Statejob(); 
