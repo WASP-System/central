8c09c6bc4487186f1806af6602cb61a599d2903f
Ed Cheng <ed@boylesoftware.com>
2011-12-14 05:19:26 +0000

    create sample state

---
+++ src/main/java/edu/yu/einstein/wasp/batch/CreateSampleStateProcessor.java
@@ -0,0 +1,73 @@
+package edu.yu.einstein.wasp.batch;
+
+import edu.yu.einstein.wasp.model.*;
+import edu.yu.einstein.wasp.service.*;
+
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.support.transaction.FlushFailedException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.springframework.stereotype.Component;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Date;
+
+/**
+ * Created State Processor
+ * simply creates a new State with targetTask and copies
+ * the original stated from state Sample and creates a state sample out of it.
+ * 
+ */
+
+@Component
+public class CreateSampleStateProcessor implements ItemProcessor {
+
+  @Autowired
+  StateService stateService;
+
+  @Autowired
+  TaskService taskService;
+
+  @Autowired
+  StatesampleService statesampleService;
+
+  String targetTask; 
+  public void setTargetTask(String targetTask) {
+    this.targetTask = targetTask; 
+  }
+
+  String targetStatus = "CREATED"; 
+
+  public State process(Object stateId) throws Exception {
+    
+    State state = stateService.getStateByStateId(((Integer) stateId).intValue());
+
+System.out.println("\nCreating " + targetTask + " for " + stateId);
+
+    List<Statesample> stateSamples = state.getStatesample();
+
+    Task t = taskService.getTaskByIName(targetTask); 
+
+    State newState = new State();
+    newState.setStatus(targetStatus);
+    newState.setTaskId(t.getTaskId());
+    newState.setName(t.getName());
+    newState.setStartts(new Date());
+    State newStateDb = stateService.save(newState);
+
+    for (Statesample ss: stateSamples) {
+      Statesample newStateSample = new Statesample(); 
+      newStateSample.setStateId(newStateDb.getStateId());
+      newStateSample.setSampleId(ss.getSampleId());
+
+      statesampleService.save(newStateSample);
+    }
+
+    return newState;
+  }
+}
+
