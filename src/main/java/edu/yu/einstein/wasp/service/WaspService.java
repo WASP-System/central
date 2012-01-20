bda623d3df87d251704a5c2390946168ccb8edae
Ed Cheng <ed@boylesoftware.com>
2012-01-20 22:08:55 +0000

    added interface

--- src/main/java/edu/yu/einstein/wasp/service/WaspService.java
+++ src/main/java/edu/yu/einstein/wasp/service/WaspService.java
@@ -27,6 +27,8 @@ public interface WaspService<E> {
  public Integer removeAll();
 
  public List findByMap(Map m);
+ public List findByMapExcept(Map m);
+
  public List findDistinctOrderBy(final String distinctColumnName, final String direction);
  public List findDistinctOrderBy(final String distinctColumnName, final String orderByColumnName, final String direction);
  public List findAllOrderBy(final String orderByColumnName, final String direction);
