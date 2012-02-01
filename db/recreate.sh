d01837d95d220b9dafda79f012166e19dfa7ca5e
Ed Cheng <ed@boylesoftware.com>
2012-02-01 00:17:03 +0000

    initial ci, to recreate a bare db.

---
+++ db/recreate.sh
@@ -0,0 +1,11 @@
+#!/bin/sh 
+
+
+echo drop database wasp | mysql -u root --password=2011season
+echo create database wasp | mysql -u root --password=2011season
+
+mysql -u wasp --password=waspV2 wasp < create.sql
+mysql -u wasp --password=waspV2 wasp < spring_batch_init.sql
+mysql -u wasp --password=waspV2 wasp < ../src/main/resources/uifield.update.sql
+ 
+
