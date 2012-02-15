9a08a2f9794d9d02f6aecedf785ffa3699ae6e9a
Ed Cheng <ed@boylesoftware.com>
2012-02-15 17:49:59 +0000

    recreate new location for uifields

--- db/recreate.sh
+++ db/recreate.sh
@@ -6,6 +6,6 @@ echo create database wasp | mysql -u root --password=2011season
 
 mysql -u wasp --password=waspV2 wasp < create.sql
 mysql -u wasp --password=waspV2 wasp < spring_batch_init.sql
-mysql -u wasp --password=waspV2 wasp < ../src/main/resources/uifield.update.sql
+mysql -u wasp --password=waspV2 wasp < ../wasp-core/src/main/resources/uifield.update.sql
  
 
