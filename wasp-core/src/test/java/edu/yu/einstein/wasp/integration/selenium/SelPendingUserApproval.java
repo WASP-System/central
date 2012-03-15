7b01044507dc94ab3abeaa0cc8d11920159ef81c
Natalia Volvova <natalia.volnova@einstein.yu.edu>
2012-03-15 20:02:07 +0000

    minor changes

--- wasp-core/src/test/java/edu/yu/einstein/wasp/integration/selenium/SelPendingUserApproval.java
+++ wasp-core/src/test/java/edu/yu/einstein/wasp/integration/selenium/SelPendingUserApproval.java
@@ -20,7 +20,15 @@ public class SelPendingUserApproval extends SelBaseTest{
   public void beforeClass() {
 	  
   }
-  
+  /**
+   * 
+   * @param sUserName
+   * @param sUserPass
+   * @param sExpectedUrl
+   * @param sUserEmail
+   * @param sApprovedUrl
+   * @throws Exception
+   */
   @Test (groups = "integration-tests",  dataProvider = "DP1")
   public void pendingUserApprove(String sUserName, String sUserPass, String sExpectedUrl, String sUserEmail, String sApprovedUrl) throws Exception {   
 		
@@ -33,29 +41,41 @@ public class SelPendingUserApproval extends SelBaseTest{
 	  if (!element.isDisplayed())  driver.findElement(By.xpath("//a[contains(@href, '#tabs-labUtils')]")).click();
 
 	  driver.findElement(By.xpath("//a[contains(@href,'/wasp/lab/pendinglmapproval/')]")).click();
-	  Assert.assertNotNull(driver.findElement(By.xpath("//a[contains(.,'"+sUserEmail+"')]")), "");
+	  Assert.assertNotNull(driver.findElement(By.xpath("//a[contains(.,'"+sUserEmail+"')]")), "Could not locate user with '"+sUserEmail+"' email.");
 	  driver.findElement(By.xpath("//a[contains(.,'"+sUserEmail+"')]")).click();
 	  
-	  Assert.assertNotNull(driver.findElement(By.xpath("//a[contains(.,'"+sUserEmail+"')]/div/div/")), "");
-	  Assert.assertNotNull(driver.findElement(By.xpath("//h4/a[contains(.,'"+sUserEmail+"')]/div/div/a[contains(text(),'APPROVE')]")));
-	  driver.findElement(By.xpath("///a[contains(text(),'APPROVE')]")).click();
-	  //Assert.assertTrue(driver.findElements(By.xpath("//div[contains(.,'"+sUserEmail+"')]/a[contains(.,'APPROVE')]")).size() == 0, "Failed to approve a new user");
+	  Assert.assertNotNull(driver.findElement(By.xpath("//div[contains(@id, 'accordion')]//a[contains(.,'"+sUserEmail+"')]/../../div/div/a[contains(., 'APPROVE')]")), "");
+	  driver.findElement(By.xpath("//div[contains(@id, 'accordion')]//a[contains(.,'"+sUserEmail+"')]/../../div/div/a[contains(., 'APPROVE')]")).click();
+	  Assert.assertTrue(driver.findElements(By.xpath("//div[contains(@id, 'accordion')]//a[contains(.,'"+sUserEmail+"')]/../../div/div/a[contains(., 'APPROVE')]")).size() == 0, "Failed to approve a new user");
       
   }
   
+  /**
+   * 
+   * @param sUserName
+   * @param sUserPass
+   * @param sExpectedUrl
+   * @param sUserEmail
+   * @param sRejectedUrl
+   * @throws Exception
+   */
   @Test  (groups = "integration-tests",  dataProvider = "DP2")
   public void pendingUserReject(String sUserName, String sUserPass, String sExpectedUrl, String sUserEmail, String sRejectedUrl) throws Exception { 
 	  SeleniumHelper.login(sUserName, sUserPass, driver);	 
 	  Assert.assertNotNull(driver.findElement(By.xpath("//a[contains(@href,'#tabs-labUtils')]")), "Unable to locate 'Lab Utils' tab.");
 	  driver.findElement(By.xpath("//a[contains(@href,'#tabs-labUtils')]")).click();
 
-	  Assert.assertNotNull(driver.findElement(By.xpath("//div[contains(.,'"+sUserEmail+"')]/a[contains(.,'REJECT')]")));
-     
       WebElement element = driver.findElement(By.xpath("//a[contains(@href,'#tabs-labUtils')]"));
 	  if (!element.isDisplayed())  driver.findElement(By.xpath("//a[contains(@href, '#tabs-labUtils')]")).click();
-
-  	  driver.findElement(By.xpath("//div[contains(.,'"+sUserName+"')]/a[contains(.,'REJECT')]")).click();
-	  Assert.assertTrue(driver.findElements(By.xpath("//div[contains(.,'"+sUserEmail+"')]/a[contains(.,'REJECT')]")).size() == 0, "Failed to reject a new user");
+	  
+	  driver.findElement(By.xpath("//a[contains(@href,'/wasp/lab/pendinglmapproval/')]")).click();
+	  Assert.assertNotNull(driver.findElement(By.xpath("//a[contains(.,'"+sUserEmail+"')]")), "Could not locate user with '"+sUserEmail+"' email.");
+	  driver.findElement(By.xpath("//a[contains(.,'"+sUserEmail+"')]")).click();
+	  
+	  Assert.assertNotNull(driver.findElement(By.xpath("//div[contains(@id, 'accordion')]//a[contains(.,'"+sUserEmail+"')]/../../div/div/a[contains(., 'REJECT')]")), "");
+	  driver.findElement(By.xpath("//div[contains(@id, 'accordion')]//a[contains(.,'"+sUserEmail+"')]/../../div/div/a[contains(., 'REJECT')]")).click();
+	  
+	  Assert.assertTrue(driver.findElements(By.xpath("//div[contains(@id, 'accordion')]//a[contains(.,'"+sUserEmail+"')]/../../div/div/a[contains(., 'REJECT')]")).size() == 0, "Failed to reject a new user");
 
   }
   
@@ -77,6 +97,11 @@ public class SelPendingUserApproval extends SelBaseTest{
       return(retObjArr);
   }
   
+  /**
+   * 
+   * @return
+   * @throws Exception
+   */
   @DataProvider(name = "DP2")
   public Object[][] createData2() throws Exception{
       Object[][] retObjArr=SeleniumHelper.getTableArray("WaspTestData.xls",
