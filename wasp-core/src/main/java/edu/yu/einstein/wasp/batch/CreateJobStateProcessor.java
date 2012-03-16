f1d0c076fd6c34b9df154affe56a5c4587b7bf00
R. Brent Calder <brent.calder@einstein.yu.edu>
2012-03-16 15:15:30 +0000

    removed println

--- wasp-core/src/main/java/edu/yu/einstein/wasp/batch/CreateJobStateProcessor.java
+++ wasp-core/src/main/java/edu/yu/einstein/wasp/batch/CreateJobStateProcessor.java
@@ -3,6 +3,8 @@ package edu.yu.einstein.wasp.batch;
 import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -15,8 +17,7 @@ import edu.yu.einstein.wasp.model.Statejob;
 import edu.yu.einstein.wasp.model.Task;
 
 /**
- * Created Job Processor
- * simply creates a new State with targetTask and copies
+ * Created Job Processor simply creates a new State with targetTask and copies
  * the original jobid from state Job and creates a state job out of it.
  * 
  */
@@ -24,50 +25,52 @@ import edu.yu.einstein.wasp.model.Task;
 @Component
 public class CreateJobStateProcessor implements ItemProcessor {
 
-  @Autowired
-  StateDao stateDao;
+	private final Log logger = LogFactory.getLog(getClass());
 
-  @Autowired
-  TaskDao taskDao;
+	@Autowired
+	StateDao stateDao;
 
-  @Autowired
-  StatejobDao statejobDao;
+	@Autowired
+	TaskDao taskDao;
 
-  String targetTask; 
-  public void setTargetTask(String targetTask) {
-    this.targetTask = targetTask; 
-  }
+	@Autowired
+	StatejobDao statejobDao;
 
-  TaskStatus targetStatus = TaskStatus.CREATED;
+	String targetTask;
 
-  @Override
-public State process(Object stateId) throws Exception {
-    
-    State state = stateDao.getStateByStateId(((Integer) stateId).intValue());
+	public void setTargetTask(String targetTask) {
+		this.targetTask = targetTask;
+	}
 
-System.out.println("\nCreating " + targetTask + " for " + stateId);
+	TaskStatus targetStatus = TaskStatus.CREATED;
 
-    List<Statejob> stateJobs = state.getStatejob();
+	@Override
+	public State process(Object stateId) throws Exception {
 
-    Task t = taskDao.getTaskByIName(targetTask); 
+		State state = stateDao.getStateByStateId(((Integer) stateId).intValue());
 
-    State newState = new State();
-    newState.setStatus(targetStatus.toString());
-    newState.setTaskId(t.getTaskId());
-    newState.setName(t.getName());
-    newState.setSourceStateId((Integer) stateId);
-    newState.setStartts(new Date());
-    State newStateDb = stateDao.save(newState);
+		logger.debug("Creating " + targetTask + " for " + stateId);
 
-    for (Statejob sj: stateJobs) {
-      Statejob newStateJob = new Statejob(); 
-      newStateJob.setStateId(newStateDb.getStateId());
-      newStateJob.setJobId(sj.getJobId());
+		List<Statejob> stateJobs = state.getStatejob();
 
-      statejobDao.save(newStateJob);
-    }
+		Task t = taskDao.getTaskByIName(targetTask);
 
-    return newState;
-  }
+		State newState = new State();
+		newState.setStatus(targetStatus.toString());
+		newState.setTaskId(t.getTaskId());
+		newState.setName(t.getName());
+		newState.setSourceStateId((Integer) stateId);
+		newState.setStartts(new Date());
+		State newStateDb = stateDao.save(newState);
+
+		for (Statejob sj : stateJobs) {
+			Statejob newStateJob = new Statejob();
+			newStateJob.setStateId(newStateDb.getStateId());
+			newStateJob.setJobId(sj.getJobId());
+
+			statejobDao.save(newStateJob);
+		}
+
+		return newState;
+	}
 }
-
