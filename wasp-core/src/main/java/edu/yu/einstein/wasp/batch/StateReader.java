65958e0c8e6568871a74acadf675c1b7379c94f3
R. Brent Calder <brent.calder@einstein.yu.edu>
2012-03-16 15:22:49 +0000

    reformat, logger

--- wasp-core/src/main/java/edu/yu/einstein/wasp/batch/StateReader.java
+++ wasp-core/src/main/java/edu/yu/einstein/wasp/batch/StateReader.java
@@ -4,7 +4,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.batch.item.ItemReader;
 
-
 // Generic State Reader, 
 // takes in StateIds
 // and forwards them to ItemProcessor
@@ -12,28 +11,30 @@ import org.springframework.batch.item.ItemReader;
 // @ // Component
 public class StateReader implements ItemReader {
 
-  private static final Log log = LogFactory.getLog(StateReader.class);
+	private final Log logger = LogFactory.getLog(getClass());
 
-  private int index = 0;
+	private int index = 0;
 
-  Integer stateId = null;
-  public void setStateId(Integer stateId) {
-    this.stateId = stateId;
-  }
-  public Integer getStateId() {
-    return this.stateId;
-  }
+	Integer stateId = null;
 
-  @Override
-  public Integer read() {
+	public void setStateId(Integer stateId) {
+		this.stateId = stateId;
+	}
 
-    if (this.stateId == null) return null;
+	public Integer getStateId() {
+		return this.stateId;
+	}
 
-    log.info( "Reader State: " + this.stateId );
-    Integer rt = this.getStateId(); 
-    setStateId(null); 
+	@Override
+	public Integer read() {
 
-    return rt;
-  }
+		if (this.stateId == null)
+			return null;
+
+		log.info("Reader State: " + this.stateId);
+		Integer rt = this.getStateId();
+		setStateId(null);
+
+		return rt;
+	}
 }
-
