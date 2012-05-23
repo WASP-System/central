07798c069021b3e9638890a7a038ffdd593a0e94
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2012-05-23 15:04:29 +0000

    Tidied up

--- db/v1/create_v1_db.sql
+++
@@ -1,7 +0,0 @@
-create database waspv1;
-create user waspv1;
-update mysql.user set host='localhost' where user='waspv1';
-update mysql.user set password=PASSWORD('waspV0ne') where user='waspv1';
-grant all on wasp.* to 'waspv1'@localhost;
-grant all on waspv1.* to 'waspv1'@localhost;
-flush privileges;
