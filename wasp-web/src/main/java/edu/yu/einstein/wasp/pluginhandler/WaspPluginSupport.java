7e667d87e3ee70c36631b113530cc5561dfb1152
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2012-05-24 16:53:01 +0000

    Now only sucks jsps out of wasp plugins

--- wasp-web/src/main/java/edu/yu/einstein/wasp/pluginhandler/WaspPluginSupport.java
+++ wasp-web/src/main/java/edu/yu/einstein/wasp/pluginhandler/WaspPluginSupport.java
@@ -1,4 +1,4 @@
-package edu.yu.einstein.wasp.tiles;
+package edu.yu.einstein.wasp.pluginhandler;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -6,12 +6,18 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.JarURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
-import java.util.Set;
 
 import javax.annotation.PostConstruct;
 import javax.servlet.ServletContext;
@@ -19,11 +25,6 @@ import javax.servlet.ServletContext;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.context.ServletContextAware;
 
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.web.context.support.ServletContextResourcePatternResolver;
-import org.springframework.web.context.support.ServletContextResource;
-
-import org.springframework.core.io.Resource;
 
 // based from http://stackoverflow.com/questions/5013917/can-i-serve-jsps-from-inside-a-jar-in-lib-or-is-there-a-workaround
 
@@ -31,12 +32,13 @@ import org.springframework.core.io.Resource;
  * Allows extraction of contents of a JAR file. All files matching a given Ant path pattern will be extracted into a
  * specified path.
  */
-public class WaspJspPluginSupport implements ServletContextAware {
+public class WaspPluginSupport implements ServletContextAware {
 
-	private String resourceDirectory;
+	private List<String> resourceDirectoryList;
 	private String resourceFilePattern;
 	private ServletContext servletContext;
 	private AntPathMatcher pathMatcher = new AntPathMatcher();
+	private String resourceUrlFilter;
 
 	/**
 	 * Creates a new instance of the JarFileResourcesExtractor
@@ -48,8 +50,16 @@ public class WaspJspPluginSupport implements ServletContextAware {
 	 * @param destination
 	 *			Target folder of the extracted resources. Relative to the context.
 	 */
-	private WaspJspPluginSupport(String resourceDirectory, String resourceFilePattern) {
-		this.resourceDirectory = resourceDirectory;
+	private WaspPluginSupport(String resourceDirectory, String resourceUrlFilter, String resourceFilePattern) {
+		this.resourceDirectoryList = new ArrayList<String>();
+		this.resourceDirectoryList.add(resourceDirectory);
+		this.resourceUrlFilter = resourceUrlFilter;
+		this.resourceFilePattern = resourceFilePattern;
+	}
+	
+	private WaspPluginSupport(List<String> resourceDirectory, String resourceUrlFilter, String resourceFilePattern) {
+		this.resourceDirectoryList = resourceDirectory;
+		this.resourceUrlFilter = resourceUrlFilter;
 		this.resourceFilePattern = resourceFilePattern;
 	}
 
@@ -63,33 +73,50 @@ public class WaspJspPluginSupport implements ServletContextAware {
 	 */
 	@PostConstruct
 	public void extractFiles() throws IOException {
-
-		Enumeration<java.net.URL>resources = this.getClass().getClassLoader().getResources(resourceDirectory);
-		while (resources.hasMoreElements()) {
-			java.net.URL foundMatch = resources.nextElement(); 
-
-			try {
-				java.net.JarURLConnection conn = (java.net.JarURLConnection)foundMatch.openConnection();
-				JarFile jarFile = conn.getJarFile();
-	
-				Enumeration<JarEntry> entries = jarFile.entries();
-				while (entries.hasMoreElements()) {
-					JarEntry entry = entries.nextElement();
-					if (pathMatcher.match( resourceDirectory + "/" + resourceFilePattern, entry.getName())) {
-						String fileName = entry.getName().replaceFirst(".*\\/", "");
-						File destinationFolder = new File(servletContext.getRealPath(resourceDirectory));
-						InputStream inputStream = jarFile.getInputStream(entry);
-						File materializedJsp = new File(destinationFolder, fileName);
-						FileOutputStream outputStream = new FileOutputStream(materializedJsp);
-						copyAndClose(inputStream, outputStream);
-					}
-				}
-			} catch (MalformedURLException e) {
-				throw new FileNotFoundException("Cannot find jar file in libs: " + foundMatch);
-			} catch (IOException e) {
-				throw new IOException("IOException while moving resources.", e);
+		// get list of applicable jars
+		Map<String,List<URL>> filteredResourceUrls = new HashMap<String,List<URL>>();
+		System.out.println("WaspPluginSupport: Resource URL filter: "+resourceUrlFilter);
+		for (String resourceDirectory: this.resourceDirectoryList){
+			Enumeration<URL> resources = this.getClass().getClassLoader().getResources(resourceDirectory);
+			while (resources.hasMoreElements()) {
+				URL url = resources.nextElement(); 
+				if (!url.getPath().contains(resourceUrlFilter))
+					break; // we're looking in a jar that is not a waspPlugin jar
+				if (!filteredResourceUrls.containsKey(resourceDirectory))
+					filteredResourceUrls.put(resourceDirectory, new ArrayList<URL>());
+				filteredResourceUrls.get(resourceDirectory).add(url);
 			}
 		}
+		if (filteredResourceUrls.isEmpty()){
+			System.out.println("WaspPluginSupport: No WASP plugin jars contained files in any resource directory provided");
+			return;
+		}
+		for (String resourceDirectory: filteredResourceUrls.keySet()){
+			for (URL resourceUrl: filteredResourceUrls.get(resourceDirectory)){
+				try {
+					JarURLConnection conn = (JarURLConnection)resourceUrl.openConnection();
+					JarFile jarFile = conn.getJarFile();
+					System.out.println("WaspPluginSupport: Extracting resources from "+resourceUrl.getPath());
+					Enumeration<JarEntry> entries = jarFile.entries();
+					while (entries.hasMoreElements()) {
+						JarEntry entry = entries.nextElement();
+						if (pathMatcher.match( resourceDirectory + "/" + resourceFilePattern, entry.getName())) {
+							String fileName = entry.getName().replaceFirst(".*\\/", "");
+							File destinationFolder = new File(servletContext.getRealPath(resourceDirectory));
+							InputStream inputStream = jarFile.getInputStream(entry);
+							File materializedJsp = new File(destinationFolder, fileName);
+							FileOutputStream outputStream = new FileOutputStream(materializedJsp);
+							copyAndClose(inputStream, outputStream);
+						}
+					}
+				} catch (MalformedURLException e) {
+					throw new FileNotFoundException("Cannot process '"+resourceUrl.getPath()+"': "+e.getMessage());
+				} catch (IOException e) {
+					throw new IOException("IOException while moving resources: "+e.getMessage());
+				}
+			}
+		}
+
 
 
 /*
