d639f84156ee940ce82a7c245b388182d4126fc5
R. Brent Calder <brent.calder@einstein.yu.edu>
2012-07-16 20:05:44 -0400

    adding demo files

---
+++ wasp-core/src/main/java/edu/yu/einstein/wasp/util/DemoEmail.java
@@ -0,0 +1,15 @@
+package edu.yu.einstein.wasp.util;
+
+public class DemoEmail {
+
+	private String demoEmail = "";
+
+	public String getDemoEmail() {
+		return demoEmail;
+	}
+
+	public void setDemoEmail(String demoEmail) {
+		this.demoEmail = demoEmail;
+	}
+
+}
