6532c58183479215e7b2d725f840492234f3a877
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2012-08-03 13:39:44 -0400

    removed redundant @DirtiesContext annotations on tests

--- wasp-integration/src/test/java/edu/yu/einstein/RunFlowTests.java
+++ wasp-integration/src/test/java/edu/yu/einstein/RunFlowTests.java
@@ -31,7 +31,6 @@ import edu.yu.einstein.wasp.messages.WaspStatus;
 
 
 @ContextConfiguration(locations={"classpath:test-launch-context.xml"})
-@DirtiesContext(classMode=ClassMode.AFTER_CLASS) // close application context after tests executed
 
 public class RunFlowTests extends AbstractTestNGSpringContextTests implements MessageHandler {
 	
