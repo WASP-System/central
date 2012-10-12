351f0858caf1d80142bf0fa1cf8686342a420b6b
R. Brent Calder <brent.calder@einstein.yu.edu>
2012-10-12 18:08:07 -0400

    added methods to access adaptors by adaptorid or library.

--- wasp-core/src/main/java/edu/yu/einstein/wasp/service/AdaptorService.java
+++ wasp-core/src/main/java/edu/yu/einstein/wasp/service/AdaptorService.java
@@ -14,16 +14,42 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import edu.yu.einstein.wasp.exception.MetadataException;
+import edu.yu.einstein.wasp.exception.SampleTypeException;
 import edu.yu.einstein.wasp.model.Adaptor;
+import edu.yu.einstein.wasp.model.Sample;
 
+/**
+ * @author calder
+ * 
+ */
 @Service
 public interface AdaptorService extends WaspService {
 
-	  /**
-	   * Accepts and (in-situ) sorts list of Adaptors (of a particular Adaptorset) by Adaptor barcodenumber 
-	   * @param List<Adaptr>
-	   * @return void
-	   */
-	  public void sortAdaptorsByBarcodenumber(List<Adaptor> adaptors);
-	  
+	/**
+	 * Accepts and (in-situ) sorts list of Adaptors (of a particular Adaptorset)
+	 * by Adaptor barcodenumber
+	 * 
+	 * @param List
+	 *            <Adaptr>
+	 * @return void
+	 */
+	public void sortAdaptorsByBarcodenumber(List<Adaptor> adaptors);
+
+	/**
+	 * Returns the adaptor associated with sample metadata "genericLibrary.adaptor"
+	 * 
+	 * @param adaptorId
+	 * @return
+	 */
+	public Adaptor getAdaptorByAdaptorId(Integer adaptorId);
+	
+	/**
+	 * Given a library, return the adaptor
+	 * 
+	 * @param library
+	 * @return
+	 */
+	public Adaptor getAdaptor(Sample library) throws MetadataException, SampleTypeException;
+
 }
