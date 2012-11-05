fb3d2287cd2ff488d774c0621ad54f77f19cde17
R. Brent Calder <brent.calder@einstein.yu.edu>
2012-11-05 14:25:46 -0500

    added illumina task

---
+++ plugins/illumina/src/main/java/edu/yu/einstein/wasp/plugin/wasp/illumina/IlluminaTask.java
@@ -0,0 +1,9 @@
+package edu.yu.einstein.wasp.plugin.wasp.illumina;
+
+import edu.yu.einstein.wasp.integration.messages.WaspTask;
+
+public class IlluminaTask extends WaspTask {
+	
+	public static final String DISCOVER = "discover";
+
+}
