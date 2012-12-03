1daff7c4a4eab5334bfa2b4bb2ace9dc2c19023a
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2012-12-03 16:02:46 -0500

    Fixed improper notify abort message routing

---
+++ wasp-core/src/main/java/edu/yu/einstein/wasp/integration/messages/router/SuccessOrAbortMessageRouter.java
@@ -0,0 +1,64 @@
+package edu.yu.einstein.wasp.integration.messages.router;
+
+import org.springframework.integration.Message;
+import org.springframework.integration.MessageChannel;
+import org.springframework.integration.annotation.Router;
+
+import edu.yu.einstein.wasp.integration.messages.WaspTask;
+import edu.yu.einstein.wasp.integration.messages.payload.WaspStatus;
+
+/**
+ * Routes messages with a task of NOTIFY_STATUS reporting a WaspStatus of FAILED or ABANDONED to the abort channel. All
+ * other messages go to the success channel
+ * @author andymac
+ *
+ */
+public class SuccessOrAbortMessageRouter {
+	
+	private MessageChannel successChannel;
+	
+	private MessageChannel abortChannel;
+	
+
+	public SuccessOrAbortMessageRouter(MessageChannel successChannel,	MessageChannel abortChannel) {
+		this.successChannel = successChannel;
+		this.abortChannel = abortChannel;
+	}
+
+	public MessageChannel getSuccessChannel() {
+		return successChannel;
+	}
+
+	public void setSuccessChannel(MessageChannel successChannel) {
+		this.successChannel = successChannel;
+	}
+
+	public MessageChannel getFailureChannel() {
+		return abortChannel;
+	}
+
+	public void setFailureChannel(MessageChannel failureChannel) {
+		this.abortChannel = failureChannel;
+	}
+	
+	/**
+	 * Routes messages with a task of NOTIFY_STATUS reporting a WaspStatus of FAILED or ABANDONED to the failure channel. All
+	 * other messages go to the success channel
+	 * @param message
+	 * @return
+	 */
+	@Router
+	public MessageChannel route(Message<?> message){
+		if (!WaspStatus.class.isInstance(message.getPayload()))
+			return successChannel;
+		WaspStatus status = (WaspStatus) message.getPayload();
+		if (status.isUnsuccessful() && 
+				message.getHeaders().containsKey(WaspTask.HEADER_KEY) && 
+				message.getHeaders().get(WaspTask.HEADER_KEY).equals(WaspTask.NOTIFY_STATUS) )
+			return abortChannel;
+		return successChannel;
+	}
+	
+	
+
+}
