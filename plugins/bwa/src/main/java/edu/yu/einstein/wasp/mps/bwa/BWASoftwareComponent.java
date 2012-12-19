e121e123dfc7e9a88d1de25f5e4f341384e1372b
R. Brent Calder <brent.calder@einstein.yu.edu>
2012-12-18 19:11:40 -0500

    version

--- plugins/bwa/src/main/java/edu/yu/einstein/wasp/mps/bwa/BWASoftwareComponent.java
+++ plugins/bwa/src/main/java/edu/yu/einstein/wasp/mps/bwa/BWASoftwareComponent.java
@@ -3,28 +3,27 @@
  */
 package edu.yu.einstein.wasp.mps.bwa;
 
-import edu.yu.einstein.wasp.grid.work.SoftwareComponent;
 import edu.yu.einstein.wasp.software.alignment.ReferenceBasedAligner;
 
 /**
  * @author calder
  *
  */
-public class BWASoftwareComponent extends ReferenceBasedAligner implements SoftwareComponent {
+public class BWASoftwareComponent extends ReferenceBasedAligner {
 
-	private String version = "1.8.2"; // hard coded as this is likely the final version.
+	private String version = "0.6.2"; // hard coded as this is likely the final version.
 	private String name = "bwa"; 
 	
 	/* (non-Javadoc)
 	 * @see edu.yu.einstein.wasp.grid.work.SoftwareComponent#getName()
 	 */
 	@Override
-	public String getName() {
+	public String getSoftwareName() {
 		return name;
 	}
 	
 	@Override
-	public void setName(String name) {
+	public void setSoftwareName(String name) {
 		this.name = name;
 		
 	}
