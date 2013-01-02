2ae375adb394817bfed6bc7b6fd6387a4d9707de
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2013-01-02 17:35:49 -0500

    Fixed up locale logic

--- wasp-web/src/main/java/edu/yu/einstein/wasp/service/impl/MessageServiceWebappImpl.java
+++ wasp-web/src/main/java/edu/yu/einstein/wasp/service/impl/MessageServiceWebappImpl.java
@@ -3,13 +3,13 @@ package edu.yu.einstein.wasp.service.impl;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.jsp.jstl.core.Config;
-import javax.servlet.jsp.jstl.fmt.LocalizationContext;
 
+import org.springframework.context.NoSuchMessageException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import edu.yu.einstein.wasp.service.MessageServiceWebapp;
 
@@ -21,22 +21,14 @@ public class MessageServiceWebappImpl extends MessageServiceImpl implements Mess
 	public String getMessage(String key) {
 		String message = key; // returns the original string by default
 		try {
-			/*
-			 * get request from ThreadLocal (where Spring MVC keeps it; cannot use @Autowire feature because it makes it require 
-			 * Tomcat container) Sasha
-			 */
 			HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-			LocalizationContext nb = (LocalizationContext) Config.get(request.getSession(), Config.FMT_LOCALIZATION_CONTEXT);
-			message = (String) nb.getResourceBundle().getObject(key);
-		} catch (Throwable e) {
-			logger.warn("Cannot resolve message '" + key + "' from resource bundle (" + e.getMessage() + "), will fallback to US locale");
-		}
-
-		if (message.equals(key)) {
+			message =  messageSource.getMessage(key, null, RequestContextUtils.getLocale(request));
+		} catch (Exception e) {
+			logger.warn("Cannot resolve message '" + key + "' using Locale from resource HttpServletRequest (" + e.getMessage() + "), will fallback to US locale");
 			try {
 				message = messageSource.getMessage(key, null, Locale.US); // try to fallback to US locale
-			} catch (Throwable e) {
-				logger.warn("Cannot resolve message '" + key + "' from messageSource (" + e.getMessage() + ")");
+			} catch (NoSuchMessageException nsme) {
+				logger.warn("Cannot resolve message '" + key + "' from messageSource (" + nsme.getMessage() + ")");
 			}
 		}
 		return message;
