2f2ca211d4e0b5773b6b2386f7e1ac494ba149da
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2013-01-07 18:58:04 -0500

    Add FileType loaders and related services

---
+++ wasp-core/src/main/java/edu/yu/einstein/wasp/load/FileTypeLoaderAndFactory.java
@@ -0,0 +1,64 @@
+package edu.yu.einstein.wasp.load;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.yu.einstein.wasp.load.service.SampleTypeLoadService;
+import edu.yu.einstein.wasp.model.SampleType;
+import edu.yu.einstein.wasp.model.SampleTypeCategory;
+
+/**
+ * update/inserts db copy of type SampleType from bean definition
+ */
+
+public class FileTypeLoaderAndFactory extends WaspLoader implements FactoryBean<SampleType> {
+
+	@Autowired
+	private SampleTypeLoadService sampleTypeLoadService;
+	
+	private SampleType sampleType;
+
+	private SampleTypeCategory sampleTypeCategory;
+
+	private int isActive = 1;
+
+	public int getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(int isActive) {
+		this.isActive = isActive;
+	}
+
+	public SampleTypeCategory getSampleTypeCategory() {
+		return sampleTypeCategory;
+	}
+
+	public void setSampleTypeCategory(SampleTypeCategory sampleTypeCategory) {
+		this.sampleTypeCategory = sampleTypeCategory;
+	}
+	
+	@PostConstruct
+	public void init(){
+		sampleTypeLoadService.updateUiFields(uiFields);
+		sampleType =  sampleTypeLoadService.update(iname, name, sampleTypeCategory, isActive);
+	}
+
+	@Override
+	public SampleType getObject() throws Exception {
+		return sampleType;
+	}
+
+	@Override
+	public Class<?> getObjectType() {
+		return SampleType.class;
+	}
+
+	@Override
+	public boolean isSingleton() {
+		return true;
+	}
+
+}
