4c089fc0c0f06cf7ca4f0280b0c9aa579c148376
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2013-01-07 19:12:14 -0500

    fixed bug and added database reset for FileType on load

--- wasp-web/src/main/resources/uifield_init.sql
+++ wasp-web/src/main/resources/uifield_init.sql
@@ -3,6 +3,7 @@ update software set isactive=0;
 update resourcecategory set isactive=0;
 update sampletypecategory set isactive=0;
 update sampletype set isactive=0;
+update filetype set isactive=0;
 update resourcetype set isactive=0;
 update samplesubtype set isactive=0;
 update workflow set isactive=0;
