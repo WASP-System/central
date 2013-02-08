025c62d4aa3c39997d674e0e389e6f4398b40e46
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2013-02-07 19:27:54 -0500

    add plugin functionality

---
+++ plugins/helptag/src/main/java/edu/yu/einstein/wasp/plugin/assay/WaspHelpTagPlugin.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package edu.yu.einstein.wasp.plugin.assay;
+
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.integration.MessageChannel;
+
+import edu.yu.einstein.wasp.interfaces.cli.ClientMessageI;
+import edu.yu.einstein.wasp.plugin.WaspPlugin;
+import edu.yu.einstein.wasp.plugin.WebInterfacing;
+import edu.yu.einstein.wasp.web.Hyperlink;
+
+/**
+ * @author asmclellan
+ * 
+ */
+public class WaspHelpTagPlugin extends WaspPlugin implements ClientMessageI, WebInterfacing {
+
+	private static Logger logger = LoggerFactory.getLogger(WaspHelpTagPlugin.class);
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6546551115142070980L;
+
+	public WaspHelpTagPlugin(String pluginName, Properties waspSiteProperties, MessageChannel channel) {
+		super(pluginName, waspSiteProperties, channel);
+	}
+
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Hyperlink getDescriptionPageHyperlink(){
+		return new Hyperlink("waspHelpTag.hyperlink.label", "/wasp-helptag/description.do");
+	}
+
+}
