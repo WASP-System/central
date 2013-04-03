84cc43d445b0ae97e98d920b5dc52a98acf3968d
R. Brent Calder <brent.calder@einstein.yu.edu>
2013-04-03 11:14:46 -0400

    initial ci

---
+++ plugins/mps/src/main/java/edu/yu/einstein/wasp/mps/tools/Picard.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package edu.yu.einstein.wasp.mps.tools;
+
+import edu.yu.einstein.wasp.software.alignment.BAMProcessor;
+
+/**
+ * @author calder
+ *
+ */
+public class Picard extends BAMProcessor {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2501043398244995595L;
+	
+	private String softwareName = "picard";
+
+	/* (non-Javadoc)
+	 * @see edu.yu.einstein.wasp.software.SoftwarePackage#getSoftwareVersion()
+	 */
+	@Override
+	public String getSoftwareVersion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.yu.einstein.wasp.software.SoftwarePackage#setSoftwareVersion(java.lang.String)
+	 */
+	@Override
+	public void setSoftwareVersion(String softwareVersion) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ plugins/mps/src/main/java/edu/yu/einstein/wasp/mps/tools/SAMTools.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package edu.yu.einstein.wasp.mps.tools;
+
+import edu.yu.einstein.wasp.software.alignment.BAMProcessor;
+
+/**
+ * @author calder
+ *
+ */
+public class SAMTools extends BAMProcessor {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5558053776438200566L;
+	
+	private String softwareName = "picard";
+
+	/* (non-Javadoc)
+	 * @see edu.yu.einstein.wasp.software.SoftwarePackage#getSoftwareVersion()
+	 */
+	@Override
+	public String getSoftwareVersion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.yu.einstein.wasp.software.SoftwarePackage#setSoftwareVersion(java.lang.String)
+	 */
+	@Override
+	public void setSoftwareVersion(String softwareVersion) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
