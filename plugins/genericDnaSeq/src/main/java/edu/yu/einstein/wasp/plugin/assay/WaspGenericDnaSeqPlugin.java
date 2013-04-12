32c1e674b9bac04649a4c19793ed0bef4637f40c
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2013-04-12 17:59:15 -0400

    fixed flows so that aggregation flow now works

--- plugins/genericDnaSeq/src/main/java/edu/yu/einstein/wasp/plugin/assay/WaspGenericDnaSeqPlugin.java
+++ plugins/genericDnaSeq/src/main/java/edu/yu/einstein/wasp/plugin/assay/WaspGenericDnaSeqPlugin.java
@@ -10,6 +10,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.integration.MessageChannel;
 
 import edu.yu.einstein.wasp.Hyperlink;
+import edu.yu.einstein.wasp.integration.messages.tasks.BatchJobTask;
+import edu.yu.einstein.wasp.plugin.BatchJobProviding;
 import edu.yu.einstein.wasp.plugin.WaspPlugin;
 import edu.yu.einstein.wasp.plugin.WebInterfacing;
 import edu.yu.einstein.wasp.plugin.cli.ClientMessageI;
@@ -18,7 +20,11 @@ import edu.yu.einstein.wasp.plugin.cli.ClientMessageI;
  * @author asmclellan
  * 
  */
-public class WaspGenericDnaSeqPlugin extends WaspPlugin implements ClientMessageI, WebInterfacing {
+public class WaspGenericDnaSeqPlugin extends WaspPlugin implements ClientMessageI, WebInterfacing, BatchJobProviding {
+	
+	public static final String PREPROCESS_ANALYSIS_JOB = "genericDnaSeq.library.preProcess.jobflow.v1";
+	
+	public static final String AGREGATE_ANALYSIS_JOB = "genericDnaSeq.library.aggregate.jobflow.v1";
 
 	private static Logger logger = LoggerFactory.getLogger(WaspGenericDnaSeqPlugin.class);
 
@@ -47,6 +53,23 @@ public class WaspGenericDnaSeqPlugin extends WaspPlugin implements ClientMessage
 	@Override
 	public Hyperlink getDescriptionPageHyperlink(){
 		return new Hyperlink("waspGenericDnaSeq.hyperlink.label", "/wasp-genericDnaSeq/description.do");
+	}
+
+
+	@Override
+	public String getBatchJobName(String BatchJobType) {
+		if (BatchJobTask.ANALYSIS_LIBRARY_PREPROCESS.equals(BatchJobType))
+			return PREPROCESS_ANALYSIS_JOB;
+		else if (BatchJobTask.ANALYSIS_AGGREGATE.equals(BatchJobType))
+			return AGREGATE_ANALYSIS_JOB;
+		return null;
+	}
+
+
+	@Override
+	public String getBatchJobNameByArea(String BatchJobType, String area) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
