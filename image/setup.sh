d61d77f20f79f9a345755c8846b0bb0ea748110e
R. Brent Calder <brent.calder@einstein.yu.edu>
2013-04-19 20:25:38 -0400

    fixes

--- image/setup.sh
+++ image/setup.sh
@@ -3,8 +3,12 @@
 
 build_wasp(){
 
+  # These commands will be run as wasp user
+
   echo "
 export CATALINA_HOME=$CATALINA_HOME" >> /home/wasp/.profile
+
+  /usr/bin/mysqladmin -u root password 'wasp'
 
   #will need to automate this to get specific tags (most recent? git describe --abbrev=0 --tags )
 
@@ -19,13 +23,16 @@ export CATALINA_HOME=$CATALINA_HOME" >> /home/wasp/.profile
     w=(${W//|/ })	
     git clone https://github.com/WASP-System/${w[0]}.git
     cd ${w[0]}
-    if [ "${w[1]}" != "master" ]; then
-    	git checkout ${w[1]}
-    fi
+    #if [ "${w[1]}" != "master" ]; then
+    #	git checkout ${w[1]}
+    #fi
     #switch tag
     mvn clean install -Dcatalina.home=$CATALINA_HOME -DskipTests=true
     cd ..
   done
+
+  cd central/wasp-web
+  mvn tomcat:redeploy
 
 }
 
@@ -36,6 +43,9 @@ usermod -G tomcat7 -a wasp
 mkdir -p ${CATALINA_HOME}/waspPlugins
 chown tomcat7:tomcat7 ${CATALINA_HOME}/waspPlugins
 chmod 775 ${CATALINA_HOME}/waspPlugins/
+chown tomcat7:tomcat7 /usr/share/tomcat7/lib
+chmod 775 /usr/share/tomcat7/lib
+ln -s /usr/share/tomcat7/lib ${CATALINA_HOME}/lib
 
 export -f build_wasp 
 su wasp -c "bash -c build_wasp"
