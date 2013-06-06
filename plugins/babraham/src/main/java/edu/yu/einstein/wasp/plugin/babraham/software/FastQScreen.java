019b2dd5e2f7c3835628e70689163b326836d6c4
R. Brent Calder <brent.calder@einstein.yu.edu>
2013-06-06 16:36:26 -0400

    some eclipse files and new plugin Babraham for QC on fastq file groups

---
+++ plugins/babraham/src/main/java/edu/yu/einstein/wasp/plugin/babraham/software/FastQScreen.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package edu.yu.einstein.wasp.plugin.babraham.software;
+
+import edu.yu.einstein.wasp.grid.work.GridResult;
+import edu.yu.einstein.wasp.grid.work.WorkUnit;
+import edu.yu.einstein.wasp.model.FileGroup;
+import edu.yu.einstein.wasp.software.SoftwarePackage;
+
+/**
+ * @author calder
+ *
+ */
+public class FastQScreen extends SoftwarePackage {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2863597794926320890L;
+
+	/**
+	 * 
+	 */
+	public FastQScreen() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/** 
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String getSoftwareVersion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/** 
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void setSoftwareVersion(String softwareVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public WorkUnit getFastQScreen(FileGroup fileGroup) {
+		return null;
+	}
+	
+	public String parseOutput(GridResult result) {
+		return null;
+	}
+}
