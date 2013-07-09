38ba632bf70b7c0d4f4fd140ca65148429df3bfa
R. Brent Calder <brent.calder@einstein.yu.edu>
2013-07-09 12:36:22 -0400

    work in progress

---
+++ wasp-core/src/main/java/edu/yu/einstein/wasp/grid/GridAuthenticationException.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package edu.yu.einstein.wasp.grid;
+
+/**
+ * @author calder
+ *
+ */
+public class GridAuthenticationException extends GridAccessException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2097677446979883357L;
+
+	/**
+	 * 
+	 */
+	public GridAuthenticationException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 */
+	public GridAuthenticationException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param message
+	 * @param cause
+	 */
+	public GridAuthenticationException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
