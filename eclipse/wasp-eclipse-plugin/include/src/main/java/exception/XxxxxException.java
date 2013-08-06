c90c420d97fd975e40a5b10f6b0c81d1b1c9ae6e
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2013-08-06 17:51:51 -0400

    added exceptions

---
+++ eclipse/wasp-eclipse-plugin/include/src/main/java/exception/XxxxxException.java
@@ -0,0 +1,24 @@
+package ___package___.___pluginname___.exception;
+
+import edu.yu.einstein.wasp.exception.WaspException;
+
+/**
+ * Checked exception ___Pluginname___Exception
+ */
+public class ___Pluginname___Exception extends WaspException {
+
+	public ___Pluginname___Exception() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ___Pluginname___Exception(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ___Pluginname___Exception(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ eclipse/wasp-eclipse-plugin/include/src/main/java/exception/XxxxxRuntimeException.java
@@ -0,0 +1,24 @@
+package ___package___.___pluginname___.exception;
+
+import edu.yu.einstein.wasp.exception.WaspRuntimeException;
+
+/**
+ * Checked exception ___Pluginname___Exception
+ */
+public class ___Pluginname___RuntimeException extends WaspRuntimeException {
+
+	public ___Pluginname___RuntimeException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ___Pluginname___RuntimeException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ___Pluginname___RuntimeException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
