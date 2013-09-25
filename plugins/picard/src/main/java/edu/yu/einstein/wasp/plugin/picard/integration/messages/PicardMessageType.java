0b63d6648e0726a0b8917d45a53630c861faba39
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2013-09-25 10:12:37 -0400

    picard plugin added

---
+++ plugins/picard/src/main/java/edu/yu/einstein/wasp/plugin/picard/integration/messages/PicardMessageType.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Wasp System Eclipse Plugin
+ * @author
+ */
+package edu.yu.einstein.wasp.plugin.picard.integration.messages;
+
+import edu.yu.einstein.wasp.integration.messages.WaspMessageType;
+
+/**
+ * @author 
+ * 
+ */
+public class PicardMessageType extends WaspMessageType{
+	
+	public static final String PICARD = "edu.yu.einstein.wasp.plugin.picard";
+
+}
