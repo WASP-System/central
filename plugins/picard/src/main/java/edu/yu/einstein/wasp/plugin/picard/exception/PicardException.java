4502fa1f054381189d58b941282d65f50fa8efd8
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2013-09-30 17:51:28 -0400

    Added back corrected picard plugin

---
+++ plugins/picard/src/main/java/edu/yu/einstein/wasp/plugin/picard/exception/PicardException.java
@@ -0,0 +1,25 @@
+package edu.yu.einstein.wasp.plugin.picard.exception;
+
+import edu.yu.einstein.wasp.exception.WaspException;
+
+/**
+ * Checked exception PicardException
+ * @author 
+ */
+public class PicardException extends WaspException {
+
+	public PicardException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PicardException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PicardException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
