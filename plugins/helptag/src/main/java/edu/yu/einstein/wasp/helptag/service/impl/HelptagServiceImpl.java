75f37501b57a4e3977ab005edda635c6c5cec643
AJ Jing <qiang.jing@einstein.yu.edu>
2013-10-10 15:22:36 -0400

    First working copy of the helptag plugin, front end tested

--- plugins/helptag/src/main/java/edu/yu/einstein/wasp/helptag/service/impl/HelptagServiceImpl.java
+++ plugins/helptag/src/main/java/edu/yu/einstein/wasp/helptag/service/impl/HelptagServiceImpl.java
@@ -7,13 +7,23 @@ package edu.yu.einstein.wasp.helptag.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import edu.yu.einstein.wasp.dao.FileTypeDao;
 import edu.yu.einstein.wasp.helptag.service.HelptagService;
+import edu.yu.einstein.wasp.model.FileType;
 
 import edu.yu.einstein.wasp.service.impl.WaspServiceImpl;
 
 @Service
 @Transactional("entityManager")
 public class HelptagServiceImpl extends WaspServiceImpl implements HelptagService {
+	
+	@Autowired
+	private FileType bamFileType;
+	
+	@Autowired
+	private FileTypeDao fileTypeDao;
+
 	
 	/**
 	 * {@inheritDoc}
