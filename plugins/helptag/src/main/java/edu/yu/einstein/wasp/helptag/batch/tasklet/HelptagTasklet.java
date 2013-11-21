c5d0ee31ea77831d847ede98cf64d63fb06a02d2
AJ Jing <qiang.jing@einstein.yu.edu>
2013-11-21 14:37:48 -0500

    save a copy

--- plugins/helptag/src/main/java/edu/yu/einstein/wasp/helptag/batch/tasklet/HelptagTasklet.java
+++ plugins/helptag/src/main/java/edu/yu/einstein/wasp/helptag/batch/tasklet/HelptagTasklet.java
@@ -3,8 +3,16 @@
  */
 package edu.yu.einstein.wasp.helptag.batch.tasklet;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,16 +24,25 @@ import edu.yu.einstein.wasp.grid.work.GridResult;
 import edu.yu.einstein.wasp.grid.work.WorkUnit;
 import edu.yu.einstein.wasp.helptag.service.HelptagService;
 import edu.yu.einstein.wasp.helptag.software.Helptag;
+import edu.yu.einstein.wasp.integration.messages.WaspSoftwareJobParameters;
+import edu.yu.einstein.wasp.model.FileGroup;
+import edu.yu.einstein.wasp.model.FileType;
+import edu.yu.einstein.wasp.model.Job;
+import edu.yu.einstein.wasp.model.SampleSource;
 import edu.yu.einstein.wasp.service.FileService;
+import edu.yu.einstein.wasp.service.SampleService;
 
 /**
  * @author AJ
  *
  */
-public class HelptagTasklet extends WaspTasklet {
+public class HelptagTasklet extends WaspTasklet  implements StepExecutionListener {
 
 	@Autowired
 	private FileService fileService;
+
+	@Autowired
+	private SampleService sampleService;
 	
 	@Autowired
 	private HelptagService helptagService;
@@ -34,9 +51,13 @@ public class HelptagTasklet extends WaspTasklet {
 	private Helptag helptag;
 	
 	@Autowired
-	private GridHostResolver hostResolver;
+	private GridHostResolver gridHostResolver;
 
-	private Integer filegroupId;
+	@Autowired
+	private FileType fastqFileType;
+
+	private StepExecution stepExecution;
+	private Integer cellLibraryId;
 	
 	/**
 	 * 
@@ -45,9 +66,10 @@ public class HelptagTasklet extends WaspTasklet {
 		// proxy
 	}
 
-	public HelptagTasklet(String filegroupId) {
-		Assert.assertParameterNotNull(filegroupId);
-		this.filegroupId = Integer.valueOf(filegroupId);
+	public HelptagTasklet(String cellLibraryIds) {
+		List<Integer> cids = WaspSoftwareJobParameters.getLibraryCellIdList(cellLibraryIds);
+		Assert.assertTrue(cids.size() == 1);
+		this.cellLibraryId = cids.get(0);
 	}
 
 	/**
@@ -66,16 +88,67 @@ public class HelptagTasklet extends WaspTasklet {
 			return RepeatStatus.FINISHED;
 		}
 		
-		// get work unit
-		WorkUnit w = helptag.getHelptag(filegroupId);
+		SampleSource cellLib = sampleService.getSampleSourceDao().findById(cellLibraryId);
 		
-		// execute it
-		GridResult result = hostResolver.execute(w);
+		ExecutionContext stepContext = this.stepExecution.getExecutionContext();
+		stepContext.put("cellLibId", cellLib.getId()); //place in the step context
+		
+		Job job = sampleService.getJobOfLibraryOnCell(cellLib);
+		
+		logger.debug("Beginning HelpTagging step 1 (generate hcount file) for cellLibrary " + cellLib.getId() + " from job " + job.getId());
+		
+		Set<FileGroup> fileGroups = fileService.getFilesForCellLibraryByType(cellLib, fastqFileType); // TODO: change to bamFileType later
+		
+		logger.debug("ffileGroups.size()="+fileGroups.size());
+		Assert.assertTrue(fileGroups.size() == 1);
+		FileGroup fg = fileGroups.iterator().next();
+		
+		logger.debug("file group: " + fg.getId() + ":" + fg.getDescription());
+		
+		Map<String,Object> jobParameters = context.getStepContext().getJobParameters();
+		
+		for (String key : jobParameters.keySet()) {
+			logger.debug("Key: " + key + " Value: " + jobParameters.get(key).toString());
+		}
+		
+		WorkUnit w = helptag.getHelptag(cellLib, fg);
+		w.setResultsDirectory(WorkUnit.RESULTS_DIR_PLACEHOLDER + "/" + job.getId());
+   
+		GridResult result = gridHostResolver.execute(w);
 		
 		//place the grid result in the step context
 		storeStartedResult(context, result);
 		
+		// place scratch directory in step execution context, to be promoted
+		// to the job context at run time.
+        stepContext.put("scrDir", result.getWorkingDirectory());
+        stepContext.put("createHcountName", result.getId());
+
 		return RepeatStatus.CONTINUABLE;
+	}
+	
+	public static void doWork(int cellLibraryId) {
+		
+	}
+	
+	
+	/** 
+	 * {@inheritDoc}
+	 */
+	@Override
+	public ExitStatus afterStep(StepExecution arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/** 
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void beforeStep(StepExecution stepExecution) {
+		logger.debug("StepExecutionListener beforeStep saving StepExecution");
+		this.stepExecution = stepExecution;
+		
 	}
 
 }
