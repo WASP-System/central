56d6cc42edd800f7a9406684cfac70200b8d4a53
jcai <jinlu.cai@einstein.yu.edu>
2013-12-16 14:51:10 -0500

    gatk version 1 with interface

--- plugins/gatk/src/main/java/edu/yu/einstein/wasp/gatk/controller/GatkController.java
+++ plugins/gatk/src/main/java/edu/yu/einstein/wasp/gatk/controller/GatkController.java
@@ -4,13 +4,22 @@
  */
 package edu.yu.einstein.wasp.gatk.controller;
 
+import java.util.List;
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import edu.yu.einstein.wasp.controller.WaspController;
+import edu.yu.einstein.wasp.model.JobDraft;
+import edu.yu.einstein.wasp.model.User;
 import edu.yu.einstein.wasp.service.MessageServiceWebapp;
 import edu.yu.einstein.wasp.gatk.service.GatkService;
 
@@ -32,3 +41,4 @@ public class GatkController extends WaspController {
 	}
 
 }
+
