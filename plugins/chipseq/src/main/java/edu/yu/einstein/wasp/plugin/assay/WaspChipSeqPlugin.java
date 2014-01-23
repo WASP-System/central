4e70e2a54e6740236174675b0ac3c87e127be6c6
robert.dubin <robert.dubin@einstein.yu.edu>
2014-01-23 15:59:04 -0500

    got the hello world plugin method to run from the cli command line

--- plugins/chipseq/src/main/java/edu/yu/einstein/wasp/plugin/assay/WaspChipSeqPlugin.java
+++ plugins/chipseq/src/main/java/edu/yu/einstein/wasp/plugin/assay/WaspChipSeqPlugin.java
@@ -7,37 +7,87 @@ import java.util.Properties;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.integration.Message;
 import org.springframework.integration.MessageChannel;
+import org.springframework.integration.support.MessageBuilder;
 
 import edu.yu.einstein.wasp.Hyperlink;
+import edu.yu.einstein.wasp.exception.PanelException;
+import edu.yu.einstein.wasp.grid.GridHostResolver;
+import edu.yu.einstein.wasp.grid.file.GridFileService;
 import edu.yu.einstein.wasp.integration.messages.tasks.BatchJobTask;
+import edu.yu.einstein.wasp.integration.messaging.MessageChannelRegistry;
+import edu.yu.einstein.wasp.model.FileGroup;
 import edu.yu.einstein.wasp.plugin.BatchJobProviding;
 import edu.yu.einstein.wasp.plugin.WaspPlugin;
 import edu.yu.einstein.wasp.plugin.WebInterfacing;
 import edu.yu.einstein.wasp.plugin.cli.ClientMessageI;
+import edu.yu.einstein.wasp.service.WaspMessageHandlingService;
+import edu.yu.einstein.wasp.viewpanel.FileDataTabViewing;
+import edu.yu.einstein.wasp.viewpanel.PanelTab;
 
 /**
  * @author asmclellan
  * 
  */
-public class WaspChipSeqPlugin extends WaspPlugin implements ClientMessageI, WebInterfacing, BatchJobProviding {
+public class WaspChipSeqPlugin extends WaspPlugin implements 
+	BatchJobProviding,
+	WebInterfacing,
+	FileDataTabViewing,
+	ClientMessageI {
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	private static final long serialVersionUID = -6546554985142070980L;
+
+	@Autowired
+	@Qualifier("waspMessageHandlingServiceImpl") // more than one class of type WaspMessageHandlingService so must specify
+	private WaspMessageHandlingService waspMessageHandlingService;
+
+	@Autowired
+	private GridHostResolver waspGridHostResolver;
+
+	@Autowired
+	private GridFileService waspGridFileService;
+
+	@Autowired
+	private MessageChannelRegistry messageChannelRegistry;
 
 	public static final String PREPROCESS_ANALYSIS_JOB = "chipSeq.library.preProcess.jobflow.v1";
 	
-	public static final String AGREGATE_ANALYSIS_JOB = "chipSeq.library.aggregate.jobflow.v1";
-
-	private static Logger logger = LoggerFactory.getLogger(WaspChipSeqPlugin.class);
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -6546554985142070980L;
-
+	public static final String AGGREGATE_ANALYSIS_JOB = "chipSeq.library.aggregate.jobflow.v1";
+	
 	public WaspChipSeqPlugin(String iName, Properties waspSiteProperties, MessageChannel channel) {
 		super(iName, waspSiteProperties, channel);
 	}
 
+	/**
+	 * Methods with the signature: Message<String> methodname(Message<String> m)
+	 * are automatically accessible to execution by the command line.  Messages sent are generally
+	 * free text or JSON formatted data.  These methods should not implement their own functionality,
+	 * rather, they should either return information in a message (text) or trigger events through
+	 * integration messaging (e.g. launch a job).
+	 * 
+	 * @param m
+	 * @return
+	 */
+	public Message<String> helloWorld(Message<String> m) {
+		if (m.getPayload() == null || m.getHeaders().containsKey("help") || m.getPayload().toString().equals("help"))
+			return helloWorldHelp();
 
+		logger.info("******************************public method: Hello World from WaspChipSeqPlugin!");
+			
+		return (Message<String>) MessageBuilder.withPayload("sent a Hello World message from the wasp_chip_seq_plugin").build();
+	}
+	
+	private Message<String> helloWorldHelp() {
+		String mstr = "\n********************the message string set in chipseq helloWorldHelp: hello world, HELP!\n" +
+				"wasp -T chipseq -t helloWorld\n";
+		return MessageBuilder.withPayload(mstr).build();
+	}
+	
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		// TODO Auto-generated method stub
@@ -60,7 +110,21 @@ public class WaspChipSeqPlugin extends WaspPlugin implements ClientMessageI, Web
 		if (BatchJobTask.ANALYSIS_LIBRARY_PREPROCESS.equals(BatchJobType))
 			return PREPROCESS_ANALYSIS_JOB;
 		else if (BatchJobTask.ANALYSIS_AGGREGATE.equals(BatchJobType))
-			return AGREGATE_ANALYSIS_JOB;
+			return AGGREGATE_ANALYSIS_JOB;
+		return null;
+	}
+
+
+	@Override
+	public Status getStatus(FileGroup fileGroup) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public PanelTab getViewPanelTab(FileGroup fileGroup) throws PanelException {
+		// TODO Auto-generated method stub
 		return null;
 	}
 }
