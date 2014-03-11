6e90e86c2b62e0fb7ba36f3770139268491bf742
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2014-03-11 15:56:31 -0400

    added forgotten new file

---
+++ wasp-cli/src/main/java/edu/yu/einstein/wasp/cli/TemplateFileHandler.java
@@ -0,0 +1,83 @@
+package edu.yu.einstein.wasp.cli;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TemplateFileHandler {
+	
+	public static void createTemplateFile(Path path, boolean isUsingExistingCellLibraries) throws IOException{
+		Path templateFile;
+		try {
+			Files.deleteIfExists(path);
+			templateFile = Files.createFile(path);
+		} catch (IOException e) {
+			throw new IOException("Unable to create file " + path.getFileName());
+		}
+		List<String> lines = new ArrayList<>();
+		lines.add("# It is not necessary to duplicate entries, e.g. if supplying many samples for one job, a job entry will be matched with samples on subsequent lines until the next job entry is found.");
+		lines.add("# You can specify a metadata entry directly via it's key, e.g. FileMeta.area.key (where the entire metakey equals 'area.key').");
+		lines.add("# e.g. if you are providing columns for a FileGroup, any extra columns you add for FilegroupMeta etries will be linked to the FileGroup when it is created.");
+		lines.add("# to view available file types use the CLI: 'wasp -u <user> -p <pass> -list fileTypes'");
+		lines.add("#");
+		lines.add("# enter N/A for uknown values");
+		lines.add("#");
+		if (isUsingExistingCellLibraries){
+			lines.add("# You can add additional metadata columns for any of SampleSourceMeta, SampleMeta, FileGroupMeta and FileHandleMeta");
+			lines.add("# to view available cell libraries use the CLI: 'wasp -u <user> -p <pass> -list cellLibraries'");
+			lines.add("cellLibraryId,FileGroup.description,FileGroup.fileTypeId,FileHandle.fileName,FileHandle.fileURI,FileHandle.md5hash");
+		} else {
+			lines.add("# You can add additional metadata columns for any of JobMeta, SampleMeta, FileGroupMeta and FileHandleMeta");
+			lines.add("# to view available assay workflows use the CLI: 'wasp -u <user> -p <pass> -list workflows'");
+			lines.add("# to view available sample subtypes use the CLI: 'wasp -u <user> -p <pass> -list sampleSubtypes'");
+			lines.add("# to view available genome builds use the CLI: 'wasp -u <user> -p <pass> -list builds'");
+			lines.add("# or combined in one command: 'wasp -u <user> -p <pass> -list workflows,sampleSubtypes,builds'");
+			lines.add("Job.name,Job.workflowId,Sample.name,Sample.sampleSubtypeId,SampleMeta.genome.genomeString,FileGroup.description,FileGroup.fileTypeId,FileHandle.fileName,FileHandle.fileURI,FileHandle.md5hash");
+		}
+
+		try {
+			Files.write(templateFile, lines, Charset.defaultCharset());
+		} catch (IOException e) {
+			throw new IOException("Unable to write to file " + path.getFileName());
+		}
+	}
+	
+
+	public static List<List<String>> importTemplateFileData(Path path) throws IOException{
+		List<List<String>> data = new ArrayList<>();
+		List<String> lines = new ArrayList<>();
+		if (!Files.exists(path)){
+			throw new IOException("File does not exist: " + path.getFileName());
+		}
+		try {
+			lines = Files.readAllLines(path, Charset.defaultCharset());
+		} catch (IOException e) {
+			throw new IOException("Unable read file " + path.getFileName());
+		}
+		int lineCount = 0;
+		for (String line : lines){
+			if (line.trim().startsWith("#"))
+				continue;
+			data.add(new ArrayList<String>());
+			int elementCount = 0;
+			for (String element : line.split(",")){
+				if (element.isEmpty()){
+					if (lineCount <= 1)
+						throw new IOException("Neither header line or first line of data can have empty elements");
+					element = data.get(lineCount-1).get(elementCount); // value on previous line
+				}
+				else if (element.equalsIgnoreCase("N/A"))
+					element = "";
+				data.get(lineCount).add(element);
+				elementCount++;
+			}
+			lineCount++;
+		}
+		return data;
+	}
+
+
+}
