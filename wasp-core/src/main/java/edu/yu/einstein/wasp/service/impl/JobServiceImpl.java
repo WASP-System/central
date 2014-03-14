b777e9b9c12863a9aa015e0db18e6d970d58f316
AJ Jing <qiang.jing@einstein.yu.edu>
2014-03-14 11:49:55 -0400

    * Fixed minor bug in jobServiceImpl

--- wasp-core/src/main/java/edu/yu/einstein/wasp/service/impl/JobServiceImpl.java
+++ wasp-core/src/main/java/edu/yu/einstein/wasp/service/impl/JobServiceImpl.java
@@ -2254,7 +2254,10 @@ public static final String SAMPLE_PAIR_META_KEY = "samplePairsTvsC";
 		String workflowIname = job.getWorkflow().getIName();
 		List<JobDataTabViewing> plugins = waspPluginRegistry.getPluginsHandlingArea(workflowIname, JobDataTabViewing.class);
 		Assert.assertTrue(plugins.size()==1 || plugins.size()==0);
-		return plugins.get(0);
+		if (plugins.size()==1)
+			return plugins.get(0);
+		else
+			return null;
 	}
 
 	/**
