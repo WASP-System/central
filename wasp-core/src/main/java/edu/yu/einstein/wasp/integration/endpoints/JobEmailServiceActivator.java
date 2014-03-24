3236a47937ccc3dda48168a751db42fc79508d7f
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2014-03-24 11:16:09 -0400

    will not now try and send email if in demo mode when job submitted

--- wasp-core/src/main/java/edu/yu/einstein/wasp/integration/endpoints/JobEmailServiceActivator.java
+++ wasp-core/src/main/java/edu/yu/einstein/wasp/integration/endpoints/JobEmailServiceActivator.java
@@ -54,6 +54,9 @@ public class JobEmailServiceActivator {
 	
 	@Value("${email.sending.enabled:true}")
 	private Boolean isSendingEmailEnabled;
+	
+	@Value("${wasp.mode.isDemo:false}")
+	private Boolean isDemoMode;
 
 	private static final Logger logger = LoggerFactory.getLogger(JobEmailServiceActivator.class);
 	
@@ -70,6 +73,11 @@ public class JobEmailServiceActivator {
 	public void handleJobStatusMessage(Message<WaspStatus> jobStatusMessage) {
 		if (!isSendingEmailEnabled){
 			logger.info("Email sending is disabled so not going to send email");
+			return;
+		}
+		if (!isDemoMode){
+			// TODO: fix us so can access demo email 
+			logger.info("In Demo mode so not going to send email (email to send to exists only in web session)");
 			return;
 		}
 		if (!JobStatusMessageTemplate.isMessageOfCorrectType(jobStatusMessage)){
