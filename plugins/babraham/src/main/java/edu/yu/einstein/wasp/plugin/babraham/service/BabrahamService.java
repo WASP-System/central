6782cda480d1f926f195cb8e98e9346f94d1264e
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2014-04-04 13:38:01 -0400

    added missing file

---
+++ plugins/babraham/src/main/java/edu/yu/einstein/wasp/plugin/babraham/service/BabrahamService.java
@@ -0,0 +1,58 @@
+/**
+ * Created by Wasp System Eclipse Plugin
+ * @author 
+ */
+package edu.yu.einstein.wasp.plugin.babraham.service;
+
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import edu.yu.einstein.wasp.exception.MetadataException;
+import edu.yu.einstein.wasp.model.Software;
+import edu.yu.einstein.wasp.service.WaspService;
+
+/**
+ * 
+ */
+public interface BabrahamService extends WaspService {
+	
+		public String performAction();
+
+		/**
+		 * Save JSON representing parsed output of provided software associated with the provided fileGroup
+		 * @param JsonByKey (JSON output associated with a meta key) 
+		 * @param software (software that generated the data)
+		 * @param fileGroupId
+		 * @throws MetadataException 
+		 */
+		public void saveJsonForParsedSoftwareOutput(Map<String, JSONObject> JsonByKey, Software software, Integer fileGroupId) throws MetadataException;
+		
+		/**
+		 * Save JSON representing parsed output of provided software associated with the provided fileGroup
+		 * @param json
+		 * @param key
+		 * @param software
+		 * @param fileGroup
+		 * @throws MetadataException
+		 */
+		public void saveJsonForParsedSoftwareOutput(JSONObject json, String key, Software software, Integer fileGroupId) throws MetadataException;
+
+		/**
+		 * Retrieve JSON representing parsed output of provided software associated with the provided fileGroup
+		 * @param key
+		 * @param software (software that generated the data)
+		 * @param fileGroupId
+		 * @return (JSON output associated with a meta key)
+		 * @throws JSONException
+		 * @throws MetadataException
+		 */
+		public JSONObject getJsonForParsedSoftwareOutputByKey(String key, Software software, Integer fileGroupId) throws JSONException, MetadataException;
+
+
+		
+
+
+
+}
