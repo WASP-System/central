b06840400cf9cf25a7ad2994c23c147088c4667d
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2014-05-10 16:18:19 -0400

    Added SnpEff and VcfTools plugins

---
+++ plugins/mps/src/main/java/edu/yu/einstein/wasp/plugin/mps/grid/software/SnpEff.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Wasp System Eclipse Plugin
+ * @author
+ */
+package edu.yu.einstein.wasp.plugin.mps.grid.software;
+import edu.yu.einstein.wasp.software.SoftwarePackage;
+
+
+/**
+ * @author asmclellan
+ */
+public class SnpEff extends SoftwarePackage{
+
+	private static final long serialVersionUID = 7543620294651559883L;
+
+	public SnpEff() {
+		
+	}
+
+	
+}
---
+++ plugins/mps/src/main/java/edu/yu/einstein/wasp/plugin/mps/grid/software/VcfTools.java
@@ -0,0 +1,21 @@
+/**
+ * Created by Wasp System Eclipse Plugin
+ * @author
+ */
+package edu.yu.einstein.wasp.plugin.mps.grid.software;
+import edu.yu.einstein.wasp.software.SoftwarePackage;
+
+
+/**
+ * @author asmclellan
+ */
+public class VcfTools extends SoftwarePackage{
+
+	private static final long serialVersionUID = -2765983552942308803L;
+
+	public VcfTools() {
+		
+	}
+
+	
+}
