6249280eed61a099190beba520905845198c3099
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2014-05-23 16:43:52 -0400

    added missed package

---
+++ plugins/genericFileViewing/src/main/java/edu/yu/einstein/wasp/genericfileviewing/web/service/GenericfileviewingWebService.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Wasp System Eclipse Plugin
+ * @author 
+ */
+package edu.yu.einstein.wasp.genericfileviewing.web.service;
+
+import edu.yu.einstein.wasp.genericfileviewing.service.GenericfileviewingService;
+
+/**
+ * 
+ */
+public interface GenericfileviewingWebService extends GenericfileviewingService {
+
+}
