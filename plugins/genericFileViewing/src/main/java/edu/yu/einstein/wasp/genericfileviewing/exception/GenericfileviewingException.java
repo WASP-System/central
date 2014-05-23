85a94b1f87e40d2f9f14cd960f1e8f40044559b1
AJ Jing <qiang.jing@einstein.yu.edu>
2014-05-23 10:17:55 -0400

    * Create a plugin for generic type file viewing

---
+++ plugins/genericFileViewing/src/main/java/edu/yu/einstein/wasp/genericfileviewing/exception/GenericfileviewingException.java
@@ -0,0 +1,25 @@
+package edu.yu.einstein.wasp.genericfileviewing.exception;
+
+import edu.yu.einstein.wasp.exception.WaspException;
+
+/**
+ * Checked exception GenericfileviewingException
+ * @author 
+ */
+public class GenericfileviewingException extends WaspException {
+
+	public GenericfileviewingException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public GenericfileviewingException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GenericfileviewingException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ plugins/genericFileViewing/src/test/java/edu/yu/einstein/wasp/genericfileviewing/GenericfileviewingTestSkeleton.java
@@ -0,0 +1,60 @@
+/**
+ * Created by Wasp System Eclipse Plugin
+ * @author 
+ */
+package edu.yu.einstein.wasp.genericfileviewing;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+// The test context is created using the configuration files provided in the @ContextConfiguration locations list
+@ContextConfiguration(locations={"/genericfileviewing-test-launch-context.xml"})
+
+/**
+ * Test Skeleton
+ * @author 
+ * 
+ */
+public class GenericfileviewingTestSkeleton extends AbstractTestNGSpringContextTests{
+	
+	private final Logger logger = LoggerFactory.getLogger(GenericfileviewingTestSkeleton.class);
+	
+	/**
+	 * Code to execute before running any tests
+	 */
+	@BeforeClass 
+	private void setup(){
+
+	}
+	
+	/**
+	 * Code to execute after running all tests
+	 */
+	@AfterClass 
+	public void teardown(){
+		
+	}
+
+		
+	/**
+	 * Simple Test skeleton
+	 * @throws Exception
+	 */
+	@Test (groups = "genericfileviewing-tests")
+	public void testGenericfileviewingTestSkeleton() throws Exception{
+		try{
+			// TODO: Test logic here. 
+			Assert.assertTrue(true); // pass  default
+		} catch (Exception e){
+			logger.error("Caught unexpected exception: " + e.getLocalizedMessage());
+			throw e; // re-throw the exception
+		}
+	}
+	
+}
