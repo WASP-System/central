d1cb8aae7d2dc1e1cab6787fc2c18d1e1ffc2729
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2014-07-08 16:51:35 -0400

    refactored so that bwa mem can still calculate unique read stats. Also some functionality moved from Picard plugin to Samtools plugin

--- plugins/mps/src/main/java/edu/yu/einstein/wasp/plugin/fileformat/service/BamService.java
+++ plugins/mps/src/main/java/edu/yu/einstein/wasp/plugin/fileformat/service/BamService.java
@@ -8,6 +8,23 @@ public interface BamService extends FileTypeService {
 	public static final String FILE_AREA = "bamFile";
 	public static final String BAM_INAME = "bam";
 	
+	public static final String BAMFILE_ALIGNMENT_METRICS_META_KEY = "bamFile.alignmentMetrics";
+
+	public static final String BAMFILE_ALIGNMENT_METRIC_UNPAIRED_READS = "unpairedReads";	
+	public static final String BAMFILE_ALIGNMENT_METRIC_PAIRED_READS = "pairedReads";	
+	public static final String BAMFILE_ALIGNMENT_METRIC_UNMAPPED_READS = "unmappedReads";	
+	public static final String BAMFILE_ALIGNMENT_METRIC_UNPAIRED_READ_DUPLICATES = "unpairedReadDuplicates";	
+	public static final String BAMFILE_ALIGNMENT_METRIC_PAIRED_READ_DUPLICATES = "pairedReadDuplicates";	
+	public static final String BAMFILE_ALIGNMENT_METRIC_PAIRED_READ_OPTICAL_DUPLICATES = "pairedReadOpticalDuplicates";	
+	public static final String BAMFILE_ALIGNMENT_METRIC_FRACTION_MAPPED = "fractionMapped";	
+	public static final String BAMFILE_ALIGNMENT_METRIC_MAPPED_READS = "mappedReads";	
+	public static final String BAMFILE_ALIGNMENT_METRIC_TOTAL_READS = "totalReads";	
+	public static final String BAMFILE_ALIGNMENT_METRIC_FRACTION_DUPLICATED = "fractionDuplicated"; // duplicateReads / mappedReads	
+	public static final String BAMFILE_ALIGNMENT_METRIC_DUPLICATE_READS = "duplicateReads";//this is duplicates of the set of mapped reads
+	public static final String BAMFILE_ALIGNMENT_METRIC_UNIQUE_READS = "uniqueReads";//map to single location	
+	public static final String BAMFILE_ALIGNMENT_METRIC_UNIQUE_NONREDUNDANT_READS = "uniqueNonRedundantReads";//map to single location (duplicates removed)
+	public static final String BAMFILE_ALIGNMENT_METRIC_FRACTION_UNIQUE_NONREDUNDANT = "fractionUniqueNonRedundant"; // uniqueNonRedundantReads / uniqueReads
+	
 	public FileType getBamFileType();
 
 }
--- plugins/picard/src/main/java/edu/yu/einstein/wasp/plugin/picard/service/PicardService.java
+++ plugins/picard/src/main/java/edu/yu/einstein/wasp/plugin/picard/service/PicardService.java
@@ -4,14 +4,7 @@
  */
 package edu.yu.einstein.wasp.plugin.picard.service;
 
-import java.util.Map;
-
-import org.json.JSONObject;
-
-import edu.yu.einstein.wasp.exception.MetadataException;
-import edu.yu.einstein.wasp.grid.GridHostResolver;
 import edu.yu.einstein.wasp.model.FileGroup;
-import edu.yu.einstein.wasp.model.SampleSource;
 import edu.yu.einstein.wasp.service.WaspService;
 import edu.yu.einstein.wasp.viewpanel.PanelTab;
 
@@ -20,22 +13,6 @@ import edu.yu.einstein.wasp.viewpanel.PanelTab;
  */
 public interface PicardService extends WaspService {
 
-	public static final String BAMFILE_ALIGNMENT_METRICS_META_KEY = "bamFile.alignmentMetrics";
-
-	public static final String BAMFILE_ALIGNMENT_METRIC_UNPAIRED_READS = "unpairedReads";	
-	public static final String BAMFILE_ALIGNMENT_METRIC_PAIRED_READS = "pairedReads";	
-	public static final String BAMFILE_ALIGNMENT_METRIC_UNMAPPED_READS = "unmappedReads";	
-	public static final String BAMFILE_ALIGNMENT_METRIC_UNPAIRED_READ_DUPLICATES = "unpairedReadDuplicates";	
-	public static final String BAMFILE_ALIGNMENT_METRIC_PAIRED_READ_DUPLICATES = "pairedReadDuplicates";	
-	public static final String BAMFILE_ALIGNMENT_METRIC_PAIRED_READ_OPTICAL_DUPLICATES = "pairedReadOpticalDuplicates";	
-	public static final String BAMFILE_ALIGNMENT_METRIC_FRACTION_MAPPED = "fractionMapped";	
-	public static final String BAMFILE_ALIGNMENT_METRIC_MAPPED_READS = "mappedReads";	
-	public static final String BAMFILE_ALIGNMENT_METRIC_TOTAL_READS = "totalReads";	
-	public static final String BAMFILE_ALIGNMENT_METRIC_FRACTION_DUPLICATED = "fractionDuplicated"; // duplicateReads / mappedReads	
-	public static final String BAMFILE_ALIGNMENT_METRIC_DUPLICATE_READS = "duplicateReads";//this is duplicates of the set of mapped reads
-	public static final String BAMFILE_ALIGNMENT_METRIC_UNIQUE_READS = "uniqueReads";//map to single location	
-	public static final String BAMFILE_ALIGNMENT_METRIC_UNIQUE_NONREDUNDANT_READS = "uniqueNonRedundantReads";//map to single location (duplicates removed)
-	public static final String BAMFILE_ALIGNMENT_METRIC_FRACTION_UNIQUE_NONREDUNDANT = "fractionUniqueNonRedundant"; // uniqueNonRedundantReads / uniqueReads
 	
 		/**
 		 * Perform Service
