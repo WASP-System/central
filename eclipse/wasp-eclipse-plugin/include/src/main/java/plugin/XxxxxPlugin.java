2db289e14f8941c8a8666deaa7200c505f6bbcf5
R. Brent Calder <brent.calder@einstein.yu.edu>
2014-07-29 10:54:34 -0400

    updated paths for interfaces

--- eclipse/wasp-eclipse-plugin/include/src/main/java/plugin/XxxxxPlugin.java
+++ eclipse/wasp-eclipse-plugin/include/src/main/java/plugin/XxxxxPlugin.java
@@ -29,11 +29,11 @@ import edu.yu.einstein.wasp.integration.messages.tasks.BatchJobTask;  ///// PIP
 import edu.yu.einstein.wasp.integration.messaging.MessageChannelRegistry;
 import edu.yu.einstein.wasp.model.FileGroup; ///// VIZ
 import edu.yu.einstein.wasp.model.Software;  ///// RES
-import edu.yu.einstein.wasp.plugin.BatchJobProviding;  ///// PIP 
+import edu.yu.einstein.wasp.interfacing.plugin.BatchJobProviding;  ///// PIP 
 import edu.yu.einstein.wasp.plugin.WaspPlugin;
 import edu.yu.einstein.wasp.plugin.WebInterfacing; ///// FORM
 import edu.yu.einstein.wasp.viewpanel.FileDataTabViewing; ///// VIZ
-import edu.yu.einstein.wasp.plugin.cli.ClientMessageI;
+import edu.yu.einstein.wasp.interfacing.plugin.cli.ClientMessageI;
 import edu.yu.einstein.wasp.service.WaspMessageHandlingService;
 import edu.yu.einstein.wasp.viewpanel.PanelTab; ///// VIZ
 
