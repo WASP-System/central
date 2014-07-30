de883c70259375c40c4ab8248ec53f760b6ab5f0
R. Brent Calder <brent.calder@einstein.yu.edu>
2014-07-30 16:20:53 -0400

    more eclipse fixes and abandoned genome plugin for the moment

--- eclipse/wasp-eclipse-plugin/include/src/main/java/plugin/XxxxxPlugin.java
+++ eclipse/wasp-eclipse-plugin/include/src/main/java/plugin/XxxxxPlugin.java
@@ -19,7 +19,7 @@ import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
 
-import edu.yu.einstein.wasp.Hyperlink;  ///// FORMVIZ
+import edu.yu.einstein.wasp.interfacing.Hyperlink;  ///// FORMVIZ
 import edu.yu.einstein.wasp.exception.PanelException; ///// VIZ
 import edu.yu.einstein.wasp.exception.WaspMessageBuildingException; ///// PIP 
 import edu.yu.einstein.wasp.grid.GridHostResolver;
@@ -31,7 +31,7 @@ import edu.yu.einstein.wasp.model.FileGroup; ///// VIZ
 import edu.yu.einstein.wasp.model.Software;  ///// RES
 import edu.yu.einstein.wasp.interfacing.plugin.BatchJobProviding;  ///// PIP 
 import edu.yu.einstein.wasp.plugin.WaspPlugin;
-import edu.yu.einstein.wasp.plugin.WebInterfacing; ///// FORM
+import edu.yu.einstein.wasp.interfacingplugin.WebInterfacing; ///// FORM
 import edu.yu.einstein.wasp.viewpanel.FileDataTabViewing; ///// VIZ
 import edu.yu.einstein.wasp.interfacing.plugin.cli.ClientMessageI;
 import edu.yu.einstein.wasp.service.WaspMessageHandlingService;
