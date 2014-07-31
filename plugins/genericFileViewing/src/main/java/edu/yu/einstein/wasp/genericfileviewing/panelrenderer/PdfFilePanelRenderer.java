d6ec9ed1df80d33138b26d8243e6dbc354a6c913
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2014-07-30 20:06:56 -0400

    update panel renering for generic files

--- plugins/genericFileViewing/src/main/java/edu/yu/einstein/wasp/genericfileviewing/panelrenderer/PdfFilePanelRenderer.java
+++ plugins/genericFileViewing/src/main/java/edu/yu/einstein/wasp/genericfileviewing/panelrenderer/PdfFilePanelRenderer.java
@@ -5,7 +5,7 @@ package edu.yu.einstein.wasp.genericfileviewing.panelrenderer;
 
 import edu.yu.einstein.wasp.viewpanel.PDFContent;
 import edu.yu.einstein.wasp.viewpanel.PDFPanel;
-import edu.yu.einstein.wasp.viewpanel.Panel;
+import edu.yu.einstein.wasp.viewpanel.PanelTab;
 
 /**
  * @author aj
@@ -13,19 +13,25 @@ import edu.yu.einstein.wasp.viewpanel.Panel;
  */
 public class PdfFilePanelRenderer {
 
-	public static Panel getPanelForFileGroup(String fileURL) {
+	public static PanelTab getPanelForFileGroup(String fileName, String fileURL) {
+		PanelTab panelTab = new PanelTab();
+		panelTab.setNumberOfColumns(1);
+		panelTab.setName("PDF File Viewer");
+		panelTab.setDescription("Generic PDF file viewing");
+		panelTab.setMaxOnLoad(true);
 		PDFPanel panel = new PDFPanel();
+		panel.setTitle(fileName);
+		panelTab.addPanel(panel);
 		PDFContent content = new PDFContent();
-
 		if (fileURL == null) {
 			panel.setContent(content);
-			return panel;
+			return panelTab;
 		}
 		
 		content.setPdfURL(fileURL);
 		panel.setContent(content);
 
-		return panel;
+		return panelTab;
 	}
 
 }
