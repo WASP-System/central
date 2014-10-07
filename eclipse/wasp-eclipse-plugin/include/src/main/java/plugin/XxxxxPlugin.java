ce3e22cdf73b8dbe270a825a824cae9f6956b3bd
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2014-10-07 16:39:22 -0400

    added JobTabViewing

--- eclipse/wasp-eclipse-plugin/include/src/main/java/plugin/XxxxxPlugin.java
+++ eclipse/wasp-eclipse-plugin/include/src/main/java/plugin/XxxxxPlugin.java
@@ -8,6 +8,7 @@ import java.util.Calendar;
 import java.util.HashMap; ///// PIP
 import java.util.Map; ///// PIP
 import java.util.Properties;
+import java.util.Set;
 
 import org.json.JSONException; ///// PIP
 import org.json.JSONObject; ///// PIP
@@ -28,14 +29,17 @@ import edu.yu.einstein.wasp.integration.messages.WaspJobParameters; ///// PIP
 import edu.yu.einstein.wasp.integration.messages.tasks.BatchJobTask;  ///// PIP
 import edu.yu.einstein.wasp.integration.messaging.MessageChannelRegistry;
 import edu.yu.einstein.wasp.model.FileGroup; ///// VIZ
+import edu.yu.einstein.wasp.model.Job;
 import edu.yu.einstein.wasp.model.Software;  ///// RES
 import edu.yu.einstein.wasp.plugin.BatchJobProviding;  ///// PIP 
 import edu.yu.einstein.wasp.plugin.WaspPlugin;
 import edu.yu.einstein.wasp.plugin.WebInterfacing; ///// FORM
 import edu.yu.einstein.wasp.viewpanel.FileDataTabViewing; ///// VIZ
+import edu.yu.einstein.wasp.viewpanel.JobDataTabViewing; ///// VIZ
 import edu.yu.einstein.wasp.plugin.cli.ClientMessageI;
 import edu.yu.einstein.wasp.service.WaspMessageHandlingService;
 import edu.yu.einstein.wasp.viewpanel.PanelTab; ///// VIZ
+import edu.yu.einstein.wasp.viewpanel.DataTabViewing.Status;
 
 /**
  * @author 
@@ -45,6 +49,7 @@ public class ___PluginIName___Plugin extends WaspPlugin
 			BatchJobProviding,	///// PIP
 			WebInterfacing, ///// FORM
 			FileDataTabViewing, ///// VIZ
+			JobDataTabViewing, ///// VIZ
 			ClientMessageI {
 
 	private final Logger logger = LoggerFactory.getLogger(getClass());
@@ -186,6 +191,22 @@ public class ___PluginIName___Plugin extends WaspPlugin
 	 */
 	@Override
 	public PanelTab getViewPanelTab(FileGroup fileGroup) throws PanelException {
+		return null;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Status getStatus(Job job) {
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Set<PanelTab> getViewPanelTabs(Job job) throws PanelException {
 		return null;
 	}
 	
