bef7374b3649d013204a2ad2d7bd1f778c8955dd
robert.dubin <robert.dubin@einstein.yu.edu>
2014-10-10 15:03:43 -0400

    merging from develop

--- wasp-core/src/main/java/edu/yu/einstein/wasp/viewpanel/Action.java
+++ wasp-core/src/main/java/edu/yu/einstein/wasp/viewpanel/Action.java
@@ -1,24 +1,39 @@
 package edu.yu.einstein.wasp.viewpanel;
 
 public class Action {
-	
-	public enum CallbackFunctionType { UNKNOWN, DOWNLOAD, OPEN_IN_CSS_WIN, OPEN_IN_NEW_BROWSER_WIN }
-	
+
+	public enum CallbackFunctionType {
+		UNKNOWN, DOWNLOAD, OPEN_IN_CSS_WIN, OPEN_IN_NEW_BROWSER_WIN
+	}
+
+	public enum GroupActionAlignType {
+		LEFT, RIGHT
+	}
+
 	private String iconClassName = "";
-	
+
 	private int icnHashCode = 0;
-	
+
 	private String tooltip = "";
-	
+
 	private String callbackContent = "";
-	
+
 	private CallbackFunctionType callbackFunctionType = CallbackFunctionType.UNKNOWN;
-	
-	public Action(){
+
+	private boolean group = false;
+
+	private String groupIconClassName = "";
+
+	private String groupTooltip = "";
+
+	private GroupActionAlignType groupAlign = GroupActionAlignType.RIGHT;
+
+	public Action() {
 		super();
 	}
-	
-	public Action(String iconClassName, String tooltip, CallbackFunctionType callbackFunctionType, String callbackContent) {
+
+	public Action(String iconClassName, String tooltip,
+			CallbackFunctionType callbackFunctionType, String callbackContent) {
 		super();
 		this.iconClassName = iconClassName;
 		this.icnHashCode = iconClassName.hashCode();
@@ -27,9 +42,40 @@ public class Action {
 		this.setCallbackContent(callbackContent);
 	}
 
+	public Action(String iconClassName, String tooltip,
+			CallbackFunctionType callbackFunctionType, String callbackContent,
+			boolean group, String groupIconClassName) {
+		super();
+		this.iconClassName = iconClassName;
+		this.icnHashCode = iconClassName.hashCode();
+		this.tooltip = tooltip;
+		this.callbackFunctionType = callbackFunctionType;
+		this.setCallbackContent(callbackContent);
+		this.group = group;
+		this.groupIconClassName = groupIconClassName;
+	}
+
+	public Action(String iconClassName, String tooltip,
+			CallbackFunctionType callbackFunctionType, String callbackContent,
+			boolean group, String groupIconClassName, String groupTooltip,
+			GroupActionAlignType groupAlign) {
+		super();
+		this.iconClassName = iconClassName;
+		this.icnHashCode = iconClassName.hashCode();
+		this.tooltip = tooltip;
+		this.callbackFunctionType = callbackFunctionType;
+		this.setCallbackContent(callbackContent);
+		this.group = group;
+		this.groupIconClassName = groupIconClassName;
+		this.groupTooltip = groupTooltip;
+		this.groupAlign = groupAlign;
+	}
+
 	/**
-	 * get icon class name specified in css somewhere e.g. '.icon-group-download { background-image: url(ext/images/icons/fam/disk_multiple.png) !important;}'.
-	 * Defaults to empty string
+	 * get icon class name specified in css somewhere e.g. '.icon-group-download
+	 * { background-image: url(ext/images/icons/fam/disk_multiple.png)
+	 * !important;}'. Defaults to empty string
+	 * 
 	 * @return
 	 */
 	public String getIconClassName() {
@@ -41,7 +87,10 @@ public class Action {
 	}
 
 	/**
-	 * Set icon class name specified in css somewhere e.g. '.icon-group-download { background-image: url(ext/images/icons/fam/disk_multiple.png) !important;}'.
+	 * Set icon class name specified in css somewhere e.g. '.icon-group-download
+	 * { background-image: url(ext/images/icons/fam/disk_multiple.png)
+	 * !important;}'.
+	 * 
 	 * @param iconClassName
 	 */
 	public void setIconClassName(String iconClassName) {
@@ -51,6 +100,7 @@ public class Action {
 
 	/**
 	 * Gets value of tooltip. Defaults to empty string.
+	 * 
 	 * @return
 	 */
 	public String getTooltip() {
@@ -59,6 +109,7 @@ public class Action {
 
 	/**
 	 * Sets value of tooltip.
+	 * 
 	 * @param tooltip
 	 */
 	public void setTooltip(String tooltip) {
@@ -66,7 +117,9 @@ public class Action {
 	}
 
 	/**
-	 * Gets callback function type, e.g. UNKNOWN, DOWNLOAD, OPEN_IN_CSS_WIN, OPEN_IN_NEW_BROWSER_WIN
+	 * Gets callback function type, e.g. UNKNOWN, DOWNLOAD, OPEN_IN_CSS_WIN,
+	 * OPEN_IN_NEW_BROWSER_WIN
+	 * 
 	 * @return
 	 */
 	public CallbackFunctionType getCallbackFunctionType() {
@@ -74,16 +127,21 @@ public class Action {
 	}
 
 	/**
-	 * Sets callback function type, e.g. UNKNOWN, DOWNLOAD, OPEN_IN_CSS_WIN, OPEN_IN_NEW_BROWSER_WIN
+	 * Sets callback function type, e.g. UNKNOWN, DOWNLOAD, OPEN_IN_CSS_WIN,
+	 * OPEN_IN_NEW_BROWSER_WIN
+	 * 
 	 * @param callbackFunctionType
 	 */
-	public void setCallbackFunctionType(CallbackFunctionType callbackFunctionType) {
+	public void setCallbackFunctionType(
+			CallbackFunctionType callbackFunctionType) {
 		this.callbackFunctionType = callbackFunctionType;
 	}
 
 	/**
-	 * Get the callback content to be handled. Handling may be dependent on callback function type. May be a URL or HTML etc. 
-	 * Defaults to empty string
+	 * Get the callback content to be handled. Handling may be dependent on
+	 * callback function type. May be a URL or HTML etc. Defaults to empty
+	 * string
+	 * 
 	 * @return
 	 */
 	public String getCallbackContent() {
@@ -91,24 +149,90 @@ public class Action {
 	}
 
 	/**
-	 * Set the callback content to be handled. Handling may be dependent on callback function type. May be a URL or HTML etc. 
+	 * Set the callback content to be handled. Handling may be dependent on
+	 * callback function type. May be a URL or HTML etc.
+	 * 
 	 * @param callbackContent
 	 */
 	public void setCallbackContent(String callbackContent) {
 		this.callbackContent = callbackContent;
 	}
-	
-	@Override
-	public boolean equals(Object obj){
-		if (this == obj) return true;
-		if (obj == null) return false;
-		if (!this.getClass().isInstance(obj) && !obj.getClass().isInstance(this)) 
-			return false; // allow comparison if one class is derived from the other
-		return iconClassName.equals(((Action) obj).getIconClassName()); // iconClassName should be the same
+
+	/**
+	 * @return the group
+	 */
+	public boolean isGroup() {
+		return group;
 	}
-	
+
+	/**
+	 * @param group
+	 *            the group to set
+	 */
+	public void setGroup(boolean group) {
+		this.group = group;
+	}
+
+	/**
+	 * @return the groupIconClassName
+	 */
+	public String getGroupIconClassName() {
+		return groupIconClassName;
+	}
+
+	/**
+	 * @param groupIconClassName
+	 *            the groupIconClassName to set
+	 */
+	public void setGroupIconClassName(String groupIconClassName) {
+		this.groupIconClassName = groupIconClassName;
+	}
+
+	/**
+	 * @return the groupTooltip
+	 */
+	public String getGroupTooltip() {
+		return groupTooltip;
+	}
+
+	/**
+	 * @param groupTooltip
+	 *            the groupTooltip to set
+	 */
+	public void setGroupTooltip(String groupTooltip) {
+		this.groupTooltip = groupTooltip;
+	}
+
+	/**
+	 * @return the groupAlign
+	 */
+	public GroupActionAlignType getGroupAlign() {
+		return groupAlign;
+	}
+
+	/**
+	 * @param groupAlign
+	 *            the groupAlign to set
+	 */
+	public void setGroupAlign(GroupActionAlignType groupAlign) {
+		this.groupAlign = groupAlign;
+	}
+
 	@Override
-	public int hashCode(){
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!this.getClass().isInstance(obj)
+				&& !obj.getClass().isInstance(this))
+			return false; // allow comparison if one class is derived from the
+							// other
+		return iconClassName.equals(((Action) obj).getIconClassName()); 
+	}
+
+	@Override
+	public int hashCode() {
 		return iconClassName.hashCode();
 	}
 
