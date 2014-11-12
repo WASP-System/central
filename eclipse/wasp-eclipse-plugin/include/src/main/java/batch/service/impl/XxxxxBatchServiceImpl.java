ecbd90a1b3ddbdc1eb979538fd29f958b5366e1e
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2014-11-12 14:00:48 -0500

    updated eclipse plugin with batch service

--- eclipse/wasp-eclipse-plugin/include/src/main/java/batch/service/impl/XxxxxBatchServiceImpl.java
+++ eclipse/wasp-eclipse-plugin/include/src/main/java/batch/service/impl/XxxxxBatchServiceImpl.java
@@ -2,16 +2,16 @@
  * Created by Wasp System Eclipse Plugin
  * @author 
  */
-package ___package___.web.service.impl;
+package ___package___.batch.service.impl;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ___package___.service.impl.Abstract___PluginIName___ServiceImpl;
-import ___package___.web.service.___PluginIName___WebService;
+import ___package___.batch.service.___PluginIName___BatchService;
 
 @Service
 @Transactional("entityManager")
-public class ___PluginIName___WebServiceImpl extends Abstract___PluginIName___ServiceImpl implements ___PluginIName___WebService {
+public class ___PluginIName___BatchServiceImpl extends Abstract___PluginIName___ServiceImpl implements ___PluginIName___BatchService {
 	
 
 }
