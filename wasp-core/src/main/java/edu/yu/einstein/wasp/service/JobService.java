fe08bc507df8f56e0308db9406fd6d63654bed1e
Andy McLellan <andrew.mclellan@einstein.yu.edu>
2014-12-05 14:41:16 -0500

    updated transactional behaviour

--- wasp-core/src/main/java/edu/yu/einstein/wasp/service/JobService.java
+++ wasp-core/src/main/java/edu/yu/einstein/wasp/service/JobService.java
@@ -684,4 +684,11 @@ public interface JobService extends WaspMessageHandlingService {
 
 	public ExitStatus getJobStatus(Job job);
 	public List<String> getAllPossibleJobStatusAsString();
+
+	/**
+	 * does not extend existing transaction
+	 * @param jobId
+	 * @return
+	 */
+	public Job getJobByJobIdInDiscreteTransaction(Integer jobId);
 }
