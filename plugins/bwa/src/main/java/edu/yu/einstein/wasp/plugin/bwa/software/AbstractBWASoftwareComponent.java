d5b02485304f38112630ee5ad6fa3232b9e537ad
R. Brent Calder <brent.calder@einstein.yu.edu>
2015-04-08 15:39:49 -0400

    bwa paired end fix

--- plugins/bwa/src/main/java/edu/yu/einstein/wasp/plugin/bwa/software/AbstractBWASoftwareComponent.java
+++ plugins/bwa/src/main/java/edu/yu/einstein/wasp/plugin/bwa/software/AbstractBWASoftwareComponent.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeSet;
 
 import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.explore.wasp.ParameterValueRetrievalException;
--- plugins/bwa/src/main/java/edu/yu/einstein/wasp/plugin/bwa/software/BWAMemSoftwareComponent.java
+++ plugins/bwa/src/main/java/edu/yu/einstein/wasp/plugin/bwa/software/BWAMemSoftwareComponent.java
@@ -9,6 +9,7 @@ import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.explore.wasp.ParameterValueRetrievalException;
 import org.springframework.transaction.annotation.Transactional;
 
+import edu.yu.einstein.wasp.Assert;
 import edu.yu.einstein.wasp.grid.work.WorkUnit;
 import edu.yu.einstein.wasp.grid.work.WorkUnitGridConfiguration;
 import edu.yu.einstein.wasp.model.FileGroup;
@@ -33,13 +34,35 @@ public class BWAMemSoftwareComponent extends AbstractBWASoftwareComponent{
 		String alnOpts = getOptString("mem", jobParameters);
 		
 		String checkIndex = "if [ ! -e " + getGenomeIndexPath(getGenomeBuild(cellLibrary)) + ".bwt ]; then\n  exit 101;\nfi";
+		
+		
+		// must be 1 or 2 read-segment input
+		Integer numberOfReadSegments = fastqService.getNumberOfReadSegments(fg);
+		Assert.assertTrue(numberOfReadSegments > 0 && numberOfReadSegments <= 2);
 
 		w.setCommand(checkIndex);
 		
-		String command = "bwa mem " + alnOpts  + " -R " + this.getReadGroupString(cellLibrary) + " -t ${" + WorkUnitGridConfiguration.NUMBER_OF_THREADS + "} " + 
-				getGenomeIndexPath(getGenomeBuild(cellLibrary)) + " " +
-				"${" + WorkUnit.INPUT_FILE + "[" + WorkUnitGridConfiguration.ZERO_TASK_ARRAY_ID + "]} " +
-				"> sam.${" + WorkUnitGridConfiguration.TASK_OUTPUT_FILE + "}"; 
+		String command = "";
+		
+		if (numberOfReadSegments.equals(1)) {
+			logger.trace("perfoming single end BWA mem on cell library: " + cellLibrary.getId());
+			command = "bwa mem " + alnOpts  + " -R " + this.getReadGroupString(cellLibrary) + " -t ${" + WorkUnitGridConfiguration.NUMBER_OF_THREADS + "} " + 
+					getGenomeIndexPath(getGenomeBuild(cellLibrary)) + " " +
+					"${" + WorkUnit.INPUT_FILE + "[" + WorkUnitGridConfiguration.ZERO_TASK_ARRAY_ID + "]} " +
+					"> sam.${" + WorkUnitGridConfiguration.TASK_OUTPUT_FILE + "}"; 
+		
+		} else {
+			logger.trace("perfoming paired-end BWA mem on cell library: " + cellLibrary.getId());
+			command += "FI=$((" + WorkUnitGridConfiguration.ZERO_TASK_ARRAY_ID + "*2))\n";
+			command += "FI2=$((FI+1))\n";
+			command += "bwa mem " + alnOpts  + " -R " + this.getReadGroupString(cellLibrary) + " -t ${" + WorkUnitGridConfiguration.NUMBER_OF_THREADS + "} " + 
+					getGenomeIndexPath(getGenomeBuild(cellLibrary)) + " " +
+					"${" + WorkUnit.INPUT_FILE + "[FI]} " +
+					"${" + WorkUnit.INPUT_FILE + "[FI2]} " +
+					"> sam.${" + WorkUnitGridConfiguration.TASK_OUTPUT_FILE + "}"; 
+			// number of tasks needs to be halved!
+			w.getConfiguration().setNumberOfTasks(w.getRequiredFiles().size()/2);
+		}
 		
 		logger.debug("Will conduct bwa mem with string: " + command);
 		
